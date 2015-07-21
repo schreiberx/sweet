6829eec11b0717d9256082f64c043ce41483ba65
Martin Schreiber <schreiberx@gmail.com>
2015-07-21 12:33:00 +0100

    removed enabled gui for validation script

--- run_tests_validation/run_test_runge_kutta_timestepping.sh
+++ run_tests_validation/run_test_runge_kutta_timestepping.sh
@@ -16,7 +16,7 @@ echo "***********************************************"
 echo "TEST RK TIMESTEPPING (release) $X"
 echo "***********************************************"
 make clean
-scons --compile-program=test_runge_kutta --gui=enable --spectral-space=disable --mode=release --spectral-dealiasing=disable
+scons --compile-program=test_runge_kutta --gui=disable --spectral-space=disable --mode=release --spectral-dealiasing=disable
 EXEC="./build/example_test_runge_kutta_gui_gnu_release -N 32 -G 0 -C -1 -t 5 -a 4"
 echo "$EXEC"
 $EXEC || exit
