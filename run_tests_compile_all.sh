ee326f1f0a2f4b0b4b0078972527f746eac07197
Martin Schreiber <schreiberx@gmail.com>
2015-07-22 00:50:20 +0100

    fixed compile test script

--- run_tests_compile_all.sh
+++ run_tests_compile_all.sh
@@ -1,17 +1,14 @@
 #! /bin/bash
 
-scons --compile-program=advection --gui=disable || exit
-scons --compile-program=polvani --gui=disable --spectral-space=enable || exit
+scons --program=advection --gui=disable || exit
+scons --program=polvani --gui=disable --spectral-space=enable || exit
 
-scons --compile-program=spectral_visualization --spectral-space=enable --gui=enable || exit
+scons --program=spectral_visualization --spectral-space=enable --gui=enable || exit
 
-scons --compile-program=swe_nonstaggered_advective --gui=enable || exit
-scons --compile-program=swe_nonstaggered_advective --spectral-space=enable || exit
-scons --compile-program=swe_nonstaggered_vector_invariant --gui=enable || exit
-scons --compile-program=swe_staggered_vector_invariant --gui=enable || exit
-#scons --compile-program=swe --gui=enable || exit
+scons --program=swe_nonstaggered_advective --gui=enable || exit
+scons --program=swe_nonstaggered_advective --spectral-space=enable || exit
+scons --program=swe_nonstaggered_vector_invariant --gui=enable || exit
+scons --program=swe_staggered_vector_invariant --gui=enable || exit
 
-scons --compile-program=advection --gui=enable || exit
-
-#scons --compile-program=test_fft --gui=disable --spectral-space=enable --spectral-dealiasing=enable || exit
-scons --compile-program=test_spectral_ops --gui=disable --spectral-space=enable || exit
+scons --unit-test=test_advection --gui=enable || exit
+scons --unit-test=test_spectral_ops --gui=disable --spectral-space=enable || exit
