dc6ac17bef1634a93f7ca5449e83eeac90a3876a
Martin Schreiber <M.Schreiber@exeter.ac.uk>
2016-10-27 00:54:56 +0100

    updated norm computation scripts

--- benchmarks_sphere/h=g=r=1_f=0/compute_max_norm_all.sh
+++ benchmarks_sphere/h=g=r=1_f=0/compute_max_norm_all.sh
@@ -4,7 +4,7 @@ FILE="prog_h_t00000000000.00111111.csv"
 
 for i in linear_gaussian_*; do
 	if [ -e "$i/$FILE" ]; then
-		./compute_norm.py "linear_gaussian_dam_rk4_robert_t0.0001_stable/$FILE" "$i/$FILE"
+		./compute_max_norm.py "linear_gaussian_dam_rk4_robert_t0.0001_stable/$FILE" "$i/$FILE"
 	else
 		echo "$i/$FILE  [not found]"
 	fi
---
+++ benchmarks_sphere/h=g=r=1_f=0/compute_rms_norm_all.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+FILE="prog_h_t00000000000.00111111.csv"
+
+for i in linear_gaussian_*; do
+	if [ -e "$i/$FILE" ]; then
+		./compute_rms_norm.py "linear_gaussian_dam_rk4_robert_t0.0001_stable/$FILE" "$i/$FILE"
+	else
+		echo "$i/$FILE  [not found]"
+	fi
+done
