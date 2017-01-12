01f6053f2bc08dacab6a79c8585c02090bc23096
Martin Schreiber <M.Schreiber@exeter.ac.uk>
2017-01-12 23:06:10 +0000

    Added Banana libpfasst scripts

---
+++ local_software/install_libpfasst.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+source config.sh
+
+
+echo "*** libPFASST ***"
+if [ ! -e "$DST_DIR/lib/libpfasst.a"  -o "$1" != "" ]; then
+	BASENAME="libpfasst"
+
+	cd "$SRC_DIR"
+
+	if [ ! -e "$BASENAME" ]; then
+		git clone https://bitbucket.org/memmett/libpfasst.git || exit 1
+	fi
+
+	cd "$BASENAME"
+
+	make || exit 1
+
+	cp -v -f "./lib/libpfasst.a" "$DST_DIR/lib/"
+	echo "DONE"
+
+else
+	echo "libpfasst (Fortran PFASST) is already installed"
+fi
