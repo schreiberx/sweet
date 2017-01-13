78e705dd6ad5cb2bbbce59d32fc176fc29c1fc06
Martin Schreiber <M.Schreiber@exeter.ac.uk>
2017-01-13 22:18:18 +0000

    Updated SCONS installation script

--- local_software/install_scons.sh
+++ local_software/install_scons.sh
@@ -5,7 +5,7 @@ source config.sh
 
 echo "*** SCONS ***"
 if [ ! -e "$DST_DIR/bin/scons"  -o "$1" != "" ]; then
-	SRC_LINK="http://kent.dl.sourceforge.net/project/scons/scons/2.4.0/scons-2.4.0.tar.gz"
+	SRC_LINK="https://netix.dl.sourceforge.net/project/scons/scons/2.4.0/scons-2.4.0.tar.gz"
 	FILENAME="`basename $SRC_LINK`"
 	BASENAME="scons-2.4.0"
 
