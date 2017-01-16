147a3dd6054840d77f13a6c0a386239708d47887
Pedro Peixoto <pedrosp@ime.usp.br>
2017-01-16 15:31:22 +0000

    Added -bash specific issues

--- local_software/env_vars.sh
+++ local_software/env_vars.sh
@@ -6,16 +6,18 @@
 # It assumes that you install all your libraries in subdirectories in $HOME/local
 #
 
-if [ "`basename $0`" != "bash" ]; then
-	echo "ERROR|"
-	echo "ERROR| THIS SCRIPT MAY NOT BE EXECUTED, BUT INCLUDED IN THE ENVIRONMENT VARIABLES!"
-	echo "ERROR| Use e.g. "
-	echo "     |"
-	echo "     |    $ source ./env_vars.sh"
-	echo "     |"
-	echo "ERROR| to setup the environment variables correctly"
-	echo "ERROR|"
-	return
+if [ "#$0" != "#-bash" ]; then
+	if [ "`basename $0`" != "bash" ]; then
+		echo "ERROR|"
+		echo "ERROR| THIS SCRIPT MAY NOT BE EXECUTED, BUT INCLUDED IN THE ENVIRONMENT VARIABLES!"
+		echo "ERROR| Use e.g. "
+		echo "     |"
+		echo "     |    $ source ./env_vars.sh"
+		echo "     |"
+		echo "ERROR| to setup the environment variables correctly"
+		echo "ERROR|"
+		return
+	fi
 fi
 
 if [ "`basename $SHELL`" != "bash" ]; then
