325cce5c5f0ae54f0e6d060c16e89a983e8b6ed7
Andreas Schmitt <aschmitt@fnb.tu-darmstadt.de>
2017-01-18 15:43:55 +0100

    "fixed" the pos_x problem for SL in burgers.cpp
    Fixed by circumventing the operator+ function of PlaneData to
    keep pos_x from being transformed into spectral space

--- src/programs/burgers.cpp
+++ src/programs/burgers.cpp
@@ -286,10 +286,15 @@ public:
 			}
 		);
 
-		//Initialize arrival points with h position
-		posx_a = pos_x+0.5*simVars.disc.cell_size[0];
-		posy_a = pos_y+0.5*simVars.disc.cell_size[1];
+		// Initialize arrival points with h position
+		//posx_a = pos_x+0.5*simVars.disc.cell_size[0];
+		//posy_a = pos_y+0.5*simVars.disc.cell_size[1];
 
+      for (std::size_t idx=0; idx < planeDataConfig->physical_array_data_number_of_elements; idx++)
+      {
+         posx_a.physical_space_data[idx] = pos_x.physical_space_data[idx]+0.5*simVars.disc.cell_size[0];
+         posy_a.physical_space_data[idx] = pos_y.physical_space_data[idx]+0.5*simVars.disc.cell_size[1];
+      }
 
 		std::cout << posx_a << std::endl;
 		std::cout << "*********************************" << std::endl;
