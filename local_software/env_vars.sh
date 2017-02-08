173f87222b0bcf9bbbceda0eb529008952024468
Martin Schreiber <M.Schreiber@exeter.ac.uk>
2017-02-08 00:51:40 +0000

    fix for env_vars.sh to use on yellowstone

--- local_software/env_vars.sh
+++ local_software/env_vars.sh
@@ -8,15 +8,18 @@
 
 if [ "#$0" != "#-bash" ]; then
 	if [ "`basename $0`" != "bash" ]; then
-		echo "ERROR|"
-		echo "ERROR| THIS SCRIPT MAY NOT BE EXECUTED, BUT INCLUDED IN THE ENVIRONMENT VARIABLES!"
-		echo "ERROR| Use e.g. "
-		echo "     |"
-		echo "     |    $ source ./env_vars.sh"
-		echo "     |"
-		echo "ERROR| to setup the environment variables correctly"
-		echo "ERROR|"
-		return
+		if [ "`basename $0`" != "modules_env_yellowstone.inc" ]; then
+			echo "ERROR|"
+			echo "ERROR| >>> $0"
+			echo "ERROR| THIS SCRIPT MAY NOT BE EXECUTED, BUT INCLUDED IN THE ENVIRONMENT VARIABLES!"
+			echo "ERROR| Use e.g. "
+			echo "     |"
+			echo "     |    $ source ./env_vars.sh"
+			echo "     |"
+			echo "ERROR| to setup the environment variables correctly"
+			echo "ERROR|"
+			return
+		fi
 	fi
 fi
 
