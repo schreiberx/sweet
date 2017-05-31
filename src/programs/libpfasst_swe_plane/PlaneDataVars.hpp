4a0a86e8cc3a41f26a7697f436eac03601215409
fhamon <fhamon@lbl.gov>
2017-05-31 08:40:29 -0700

    implemented a PlaneDataVars class to store the PlaneData vars phi, u, and v

---
+++ src/programs/libpfasst_swe_plane/PlaneDataVars.hpp
@@ -0,0 +1,90 @@
+#ifndef _PLANE_DATA_VARS_HPP_
+#define _PLANE_DATA_VARS_HPP_
+
+#include <sweet/plane/PlaneData.hpp>
+#include <sweet/plane/PlaneDataConfig.hpp>
+
+// Class containing the prognotic PlaneData variables phi, u, v
+
+class PlaneDataVars {
+
+public:
+
+  // Constructor
+  PlaneDataVars(
+                PlaneDataConfig *planeDataConfig,
+		int i_level
+		)
+
+    : prog_phi(planeDataConfig),
+      prog_u(planeDataConfig),
+      prog_v(planeDataConfig),
+      flat_data_array(nullptr),
+      flat_data_array_size(0),
+      level(i_level)
+  {}
+  
+  // Destructor
+  ~PlaneDataVars()
+  {
+    if (flat_data_array != nullptr) 
+      {
+	// release the memory
+	MemBlockAlloc::free(
+			    flat_data_array, 
+			    flat_data_array_size*sizeof(double)
+			    );
+      }
+  }
+
+  void allocate_flat_data_array(int i_n_elems) 
+  {
+    if (flat_data_array == nullptr) 
+      {
+	// allocate the memory
+	flat_data_array_size = i_n_elems;
+	flat_data_array      = MemBlockAlloc::alloc<double>(
+							    i_n_elems*sizeof(double)
+							    );
+      }						   
+  }
+  
+  // getters for the PlaneData variables
+  const PlaneData& get_phi() const {return prog_phi;}
+  PlaneData&       get_phi()       {return prog_phi;}
+  const PlaneData& get_u()   const {return prog_u;}
+  PlaneData&       get_u()         {return prog_u;}
+  const PlaneData& get_v()   const {return prog_v;}
+  PlaneData&       get_v()         {return prog_v;}
+
+  // getters for the flat data array
+  double*&         get_flat_data_array()            {return flat_data_array;}
+  const int&       get_flat_data_array_size() const {return flat_data_array_size;}
+  int&             get_flat_data_array_size()       {return flat_data_array_size;}
+
+  // getters for the level 
+  const int&       get_level() const {return level;};
+
+protected:
+  
+  // geopotential 
+  PlaneData prog_phi;
+  // velocities
+  PlaneData prog_u;
+  PlaneData prog_v;
+
+  // flat data array vector (currently used to pack and unpack)
+  double *flat_data_array;
+  int     flat_data_array_size;
+  
+  // pfasst level
+  const int level;
+
+  // default constructor, copy constructor, and operator= are disabled
+  PlaneDataVars();
+  PlaneDataVars(const PlaneDataVars&);
+  PlaneDataVars& operator=(const PlaneDataVars&);
+
+};
+
+#endif
