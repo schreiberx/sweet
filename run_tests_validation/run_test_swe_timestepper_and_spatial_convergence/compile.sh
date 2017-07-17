e64ce260135e0910a136476393d610ae785aa371
Pedro Peixoto <pedrosp@ime.usp.br>
2017-07-17 16:30:42 -0300

    swe validation update

--- run_tests_validation/run_test_swe_timestepper_and_spatial_convergence/compile.sh
+++ run_tests_validation/run_test_swe_timestepper_and_spatial_convergence/compile.sh
@@ -5,4 +5,5 @@ cd ../../
 
 scons --program=swe_plane_rexi --gui=disable --plane-spectral-space=enable --mode=release --threading=omp --rexi-thread-parallel-sum=disable --compiler=gnu -j 4
 
+scons --program=swe_plane_rexi --gui=disable --plane-spectral-space=disable --mode=release --threading=omp --rexi-thread-parallel-sum=disable --compiler=gnu -j 4
 
