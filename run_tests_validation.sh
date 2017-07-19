776fab9da959aaaa9f1c06e3ead7e0c0b7c6276c
Martin Schreiber <M.Schreiber@exeter.ac.uk>
2017-07-18 23:35:21 -0600

    fix in execution of all validation benchmarks

--- run_tests_validation.sh
+++ run_tests_validation.sh
@@ -6,7 +6,7 @@ cd run_tests_validation
 
 
 # only execute scripts which do not start with an underscore
-for i in [^_]*.sh [^_]*.py; do
+for i in [^_]*.sh; do
 	echo "******************************************************"
 	echo "* Executing script $i"
 	echo "******************************************************"
