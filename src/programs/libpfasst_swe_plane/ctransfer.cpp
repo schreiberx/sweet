d7660feb1a2463140c4f3f4da2a14b4ce735af3a
fhamon <fhamon@lbl.gov>
2017-07-31 12:04:45 -0700

    cleanup in the transfer functions

--- src/programs/libpfasst_swe_plane/ctransfer.cpp
+++ src/programs/libpfasst_swe_plane/ctransfer.cpp
@@ -1,6 +1,8 @@
 #include "ctransfer.hpp"
 #include "cencap.hpp"
 
+#include <benchmarks_plane/SWEPlaneBenchmarks.hpp>
+
 extern "C"
 {
   void c_sweet_data_restrict(
@@ -11,7 +13,7 @@ extern "C"
 			     PlaneDataCtx *i_ctx,
 			     double i_t) 
   {
-    PlaneData& h_Y_fine = i_Y_fine->get_h();
+    const PlaneData& h_Y_fine = i_Y_fine->get_h();
     const PlaneData& u_Y_fine = i_Y_fine->get_u();
     const PlaneData& v_Y_fine = i_Y_fine->get_v();
 
@@ -19,62 +21,11 @@ extern "C"
     PlaneData& u_Y_coarse = io_Y_coarse->get_u();
     PlaneData& v_Y_coarse = io_Y_coarse->get_v();
 
-    // //////////////////////////////////////////////
-
-    // // Try this with and without dealiasing
-
-    // SimulationVariables* simVars(i_ctx->get_simulation_variables());
-    
-    // h_Y_coarse.physical_update_lambda_array_indices(
-    // 						 [&](int i, int j, double &o_data)
-    // 						 {
-    // 						   o_data = (i+1.0)+(j+3.0)*j;
-						   
-    // 						   /*
-    // 						   double x = (((double)i+0.5)/(double)simVars->disc.res_physical[0])*simVars->sim.domain_size[0];
-    // 						   o_data *= std::cos(((double)x)*(double)fx*M_PI*2.0/(double)simVars->disc.res_spectral[1]);
-    // 						   */
-    // 						 }
-    // 						 );
-    // // print the original data
-    // h_Y_coarse.print_physicalArrayData();
-
-    // // create a new PlaneData object and print its data
-    // PlaneData tmp(i_ctx->get_plane_data_config(i_level_coarse));
-    // tmp = h_Y_coarse;
-    // tmp.print_physicalArrayData();
-		
-    // // convert to spectral data and then back to spectral
-    // tmp.request_data_spectral();
-    // tmp.request_data_physical();
-
-    // // manually compute and output the difference between the two PlaneData objects
-    // for (std::size_t i = 0; i < i_ctx->get_plane_data_config(i_level_coarse)->physical_array_data_number_of_elements; i++)
-    //   std::cout << tmp.physical_space_data[i] - h_Y_coarse.physical_space_data[i] << std::endl;    
-    
-    // // interpolate and then restrict 
-    // h_Y_fine = h_Y_coarse.spectral_returnWithDifferentModes(i_ctx->get_plane_data_config(i_level_fine));
-    // h_Y_coarse = h_Y_fine.spectral_returnWithDifferentModes(i_ctx->get_plane_data_config(i_level_coarse));
-
-    // // print the result
-    // h_Y_coarse.print_physicalArrayData();
-    
-    // // compute the error
-    // tmp = h_Y_coarse-tmp;
-
-    // // compute the norm
-    // const double error = tmp.reduce_maxAbs();
-    
-    // // print the norm and return
-    // std::cout << "error = " << error << std::endl;
-    // exit(0);
-    
-    // //////////////////////////////////////////////
-
     // restrict the fine variables to the coarse grid and copy into the coarse variables
     h_Y_coarse = h_Y_fine.spectral_returnWithDifferentModes(i_ctx->get_plane_data_config(i_level_coarse));
     u_Y_coarse = u_Y_fine.spectral_returnWithDifferentModes(i_ctx->get_plane_data_config(i_level_coarse));
     v_Y_coarse = v_Y_fine.spectral_returnWithDifferentModes(i_ctx->get_plane_data_config(i_level_coarse));
+ 
   }  
 
   void c_sweet_data_interpolate(
