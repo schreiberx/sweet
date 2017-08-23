2dcd768154385bb15c9201ddcdc4f7ff5463d2c0
Martin Schreiber <M.Schreiber@exeter.ac.uk>
2017-08-22 22:54:58 -0600

    Added missing parameter for REXI CI

--- run_tests_validation/run_test_rexi_ci_pde.sh
+++ run_tests_validation/run_test_rexi_ci_pde.sh
@@ -17,7 +17,7 @@ echo "TEST REXI CI PDE"
 echo "***********************************************"
 #make clean
 scons --threading=omp --unit-test=test_rexi_ci_pde --gui=disable
-EXEC="./build/test_rexi_ci_pde_*_release --rexi-method=ci"
+EXEC="./build/test_rexi_ci_pde_*_release --rexi-method=ci --rexi-ci-n=64 --rexi-ci-sx=25 --rexi-ci-sy=25 -v 5"
 echo "$EXEC"
 $EXEC || exit
 
