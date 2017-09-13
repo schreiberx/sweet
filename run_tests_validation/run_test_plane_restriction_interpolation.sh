cf8c8a5d63ba772bcf961214d16b8e104853895c
Martin Schreiber <M.Schreiber@exeter.ac.uk>
2017-09-13 18:09:06 +0100

    fixed missing binary in validation script

--- run_tests_validation/run_test_plane_restriction_interpolation.sh
+++ run_tests_validation/run_test_plane_restriction_interpolation.sh
@@ -14,7 +14,7 @@ make clean
 SCONS="scons --threading=omp --unit-test=test_plane_restriction_interpolation --mode=debug --plane-spectral-dealiasing=disable"
 $SCONS
 
-./build/test_plane_restriction_interpolation_planespectral_omp_libfft_gnu_debug  -N 64 || exit 1
+./build/test_plane_restriction_interpolation_*  -N 64 || exit 1
 
 
 
@@ -22,7 +22,7 @@ make clean
 SCONS="scons --threading=omp --unit-test=test_plane_restriction_interpolation --mode=debug --plane-spectral-dealiasing=enable"
 $SCONS
 
-./build/test_plane_restriction_interpolation_planespectral_planedealiasing_omp_libfft_gnu_debug  -N 64 || exit 1
+./build/test_plane_restriction_interpolation_*  -N 64 || exit 1
 
 
 echo "***********************************************"
