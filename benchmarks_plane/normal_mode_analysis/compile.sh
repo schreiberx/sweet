13a8ff4c82e3eec64668ff0080b73032eaa03ee2
Martin Schreiber <M.Schreiber@exeter.ac.uk>
2017-10-10 22:21:10 +0100

    cleanups

--- benchmarks_plane/normal_mode_analysis/compile.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cd ../../
-scons --program=swe_rexi --mode=release --plane-spectral-space=enable --sphere-spectral-space=disable --plane-spectral-dealiasing=disable
-scons --program=swe_rexi --mode=release --plane-spectral-space=enable --sphere-spectral-space=disable --plane-spectral-dealiasing=enable
