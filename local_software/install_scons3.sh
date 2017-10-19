974eadb33e96615817adacd7f521c8c8706d5ada
Martin Schreiber <M.Schreiber@exeter.ac.uk>
2017-10-19 22:43:46 +0100

    added scons3 install script

---
+++ local_software/install_scons3.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+source config.sh
+
+
+echo "*** SCONS ***"
+if [ ! -e "$DST_DIR/bin/scons"  -o "$1" != "" ]; then
+	SRC_LINK="http://www.martin-schreiber.info/pub/sweet_local_software/scons-3.0.0.tar.gz"
+	FILENAME="`basename $SRC_LINK`"
+	BASENAME="scons-3.0.0"
+
+	cd "$SRC_DIR"
+
+	download "$SRC_LINK" "$FILENAME" || exit 1
+	tar xzf "$FILENAME" || exit 1
+	cd "$BASENAME" || exit 1
+
+	python setup.py install --prefix="$DST_DIR" || exit 1
+
+	echo "DONE"
+
+else
+	echo "SCONS is already installed"
+fi
