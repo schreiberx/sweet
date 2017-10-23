4c55d13b6d96f7a318af375342f6796b4e4a9df1
Martin Schreiber <M.Schreiber@exeter.ac.uk>
2017-10-23 15:58:42 +0200

    updated postprocessing script

--- benchmarks_plane/polvani/postprocessing.sh
+++ benchmarks_plane/polvani/postprocessing.sh
@@ -2,7 +2,7 @@
 
 for i in script_swe_plane_rexi_polvani_*/output_prog_vort_t00000000900.00000000.csv; do
 
-	../../pp_plot_quad_bwr_csv.py "$i"
+	../../scripts/pp_plot_plane_bwr_csv.py "$i"
 
 	j="${i/.csv/.png}"
 
