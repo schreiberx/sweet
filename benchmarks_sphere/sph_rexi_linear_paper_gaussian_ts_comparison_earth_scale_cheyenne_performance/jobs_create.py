f754f1ddbf363de4fb792ba402523cc620b706b8
Martin Schreiber <schreiberx@gmail.com>
2017-11-20 14:11:25 -0700

    Commit for sph-rexi linear runs on the sphere

--- benchmarks_sphere/sph_rexi_linear_paper_gaussian_ts_comparison_earth_scale_cheyenne_performance/jobs_create.py
+++ benchmarks_sphere/sph_rexi_linear_paper_gaussian_ts_comparison_earth_scale_cheyenne_performance/jobs_create.py
@@ -13,7 +13,7 @@ p = SWEETJobGeneration()
 # Cluster options
 #
 p.cluster.setupTargetMachine("cheyenne")
-p.cluster.pm_space_cores_per_mpi_rank = 4
+p.cluster.pm_space_cores_per_mpi_rank = 18
 p.cluster.pm_time_cores_per_mpi_rank = 1
 
 par_time_cores_list = [2**i for i in range(0, 10)]
@@ -24,6 +24,10 @@ par_time_cores_list = [2**i for i in range(0, 10)]
 # Compile options
 #
 
+
+#
+# PDE
+#
 p.compile.program = 'swe_sphere'
 
 p.compile.plane_or_sphere = 'sphere'
@@ -32,8 +36,22 @@ p.compile.plane_spectral_dealiasing = 'disable'
 p.compile.sphere_spectral_space = 'enable'
 p.compile.sphere_spectral_dealiasing = 'enable'
 
-p.compile.rexi_thread_parallel_sum = 'enable'
+
+#
+# Use Intel MPI Compilers
+#
+p.compile.compiler_c_exec = 'mpicc'
+p.compile.compiler_cpp_exec = 'mpicxx'
+p.compile.compiler_fortran_exec = 'mpif90'
+
+
+#
+# parallelization
+#
+#p.compile.rexi_thread_parallel_sum = 'enable'
+p.compile.rexi_thread_parallel_sum = 'disable'
 p.compile.threading = 'off'
+p.compile.sweet_mpi = 'enable'
 
 
 p.compile.compiler = 'intel'
@@ -78,66 +96,82 @@ timestep_sizes = [50, 100, 200, 400, 800]
 
 
 
-####################################
-# RK 4
-####################################
+if __name__ == "__main__":
 
-if True:
-	p.runtime.timestepping_method = 'l_erk'
-	p.cluster.pm_time_cores_per_mpi_rank = 1
+	####################################
+	# RK 4 reference
+	####################################
 
-	for p.runtime.timestepping_order in [1, 2, 4]:
-		#for p.runtime.timestep_size in [50]:
-		for p.runtime.timestep_size in timestep_sizes:
-			p.gen_script('script'+p.runtime.getUniqueID(p.compile), 'run.sh')
+	if True:
+		p.runtime.timestepping_method = 'l_erk'
+		p.cluster.pm_time_cores_per_mpi_rank = 1
 
-
-
-
-####################################
-# Crank Nicolson
-####################################
-
-if True:
-	p.runtime.timestepping_method = 'l_cn'
-	p.cluster.pm_time_cores_per_mpi_rank = 1
-
-	for p.runtime.timestepping_order in [2]:
-		for p.runtime.timestep_size in [100, 200, 400, 800]:
-		#for p.runtime.timestep_size in timestep_sizes:
-			p.gen_script('script'+p.runtime.getUniqueID(p.compile), 'run.sh')
-
-
-
-####################################
-# REXI short time steps
-####################################
-
-if True:
-	p.runtime.timestepping_method = 'l_rexi'
-
-	#for p.runtime.rexi_m in [128, 256, 512, 1024, 2048]:
-	for p.runtime.rexi_m in [512]:
-		for p.runtime.timestepping_order in [0]:
-			for p.runtime.timestep_size in [800]:
+		for p.runtime.timestepping_order in [4]:
+			for p.runtime.timestep_size in [50]:
 			#for p.runtime.timestep_size in timestep_sizes:
-				for p.cluster.par_time_cores in par_time_cores_list:
-					p.gen_script('script'+p.runtime.getUniqueID(p.compile)+'_'+p.cluster.getUniqueID(), 'run.sh')
+				p.gen_script('script_ref'+p.runtime.getUniqueID(p.compile), 'run.sh')
+
+
+	####################################
+	# RK
+	####################################
+
+	if True:
+		p.runtime.timestepping_method = 'l_erk'
+		p.cluster.pm_time_cores_per_mpi_rank = 1
+
+		for p.runtime.timestepping_order in [2]:
+			#for p.runtime.timestep_size in [50]:
+			for p.runtime.timestep_size in [50, 100, 200, 400, 800]:
+				p.gen_script('script'+p.runtime.getUniqueID(p.compile), 'run.sh')
 
 
 
-####################################
-# REXI long time step
-####################################
 
-if True:
-	p.runtime.timestepping_method = 'l_rexi'
+	####################################
+	# Crank Nicolson
+	####################################
 
-	#for p.runtime.rexi_m in [1024, 4192]:
-	for p.runtime.rexi_m in [4192]:
-		for p.runtime.timestepping_order in [0]:
-			for p.runtime.timestep_size in [129600]:
+	if True:
+		p.runtime.timestepping_method = 'l_cn'
+		p.cluster.pm_time_cores_per_mpi_rank = 1
+
+		for p.runtime.timestepping_order in [2]:
+			for p.runtime.timestep_size in [50, 100, 200, 400, 800]:
 			#for p.runtime.timestep_size in timestep_sizes:
-				for p.cluster.par_time_cores in par_time_cores_list:
-					p.gen_script('script'+p.runtime.getUniqueID(p.compile)+'_'+p.cluster.getUniqueID(), 'run.sh')
+				p.gen_script('script'+p.runtime.getUniqueID(p.compile), 'run.sh')
+
+
+
+	####################################
+	# REXI short time steps
+	####################################
+
+	if True:
+		p.runtime.timestepping_method = 'l_rexi'
+
+		#for p.runtime.rexi_m in [128, 256, 512, 1024, 2048]:
+		for p.runtime.rexi_m in [512, 1024]:
+			for p.runtime.timestepping_order in [0]:
+				for p.runtime.timestep_size in [800, 1600, 2*1600]:
+				#for p.runtime.timestep_size in timestep_sizes:
+					for p.cluster.par_time_cores in par_time_cores_list:
+						p.gen_script('script'+p.runtime.getUniqueID(p.compile)+'_'+p.cluster.getUniqueID(), 'run.sh')
+
+
+
+	####################################
+	# REXI long time step
+	####################################
+
+	if True:
+		p.runtime.timestepping_method = 'l_rexi'
+
+		#for p.runtime.rexi_m in [1024, 4192]:
+		for p.runtime.rexi_m in [4192]:
+			for p.runtime.timestepping_order in [0]:
+				for p.runtime.timestep_size in [129600]:
+				#for p.runtime.timestep_size in timestep_sizes:
+					for p.cluster.par_time_cores in par_time_cores_list:
+						p.gen_script('script'+p.runtime.getUniqueID(p.compile)+'_'+p.cluster.getUniqueID(), 'run.sh')
 
