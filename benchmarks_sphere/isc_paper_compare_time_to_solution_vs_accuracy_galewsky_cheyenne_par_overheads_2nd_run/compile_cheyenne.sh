eb6b4808060f3ae1259847ff003c75f49413c7a9
Martin Schreiber <schreiberx@gmail.com>
2017-12-10 13:22:47 -0700

    2nd run tests

--- benchmarks_sphere/isc_paper_compare_time_to_solution_vs_accuracy_galewsky_cheyenne_par_overheads_2nd_run/compile_cheyenne.sh
+++ benchmarks_sphere/isc_paper_compare_time_to_solution_vs_accuracy_galewsky_cheyenne_par_overheads_2nd_run/compile_cheyenne.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-SWEETROOT="/glade/u/home/martins/workspace/sweet/benchmarks_sphere/isc_paper_compare_time_to_solution_vs_accuracy_galewsky_cheyenne_par_overheads_2nd_run/script_ln2_g9.81_h10000_f7.2921e-05_a6371220_u0.0_U0_fsph0_tsm_lg_rexi_tso2_tsob2_C000360_REXICI_n00000128_mr10.0_mi30.0_prcircle_gfs0.0000E+00_gfd0.0000E+00_gfe0.0000E+00_nrm0_hlf0_bf0_ext00_M0128_MPI_space01_time128/../../../"
+SWEETROOT="/glade/u/home/martins/workspace/sweet/benchmarks_sphere/isc_paper_compare_time_to_solution_vs_accuracy_galewsky_cheyenne_par_overheads/script_ln2_g9.81_h10000_f7.2921e-05_a6371220_u0.0_U0_fsph0_tsm_lg_rexi_tso2_tsob2_C000360_REXICI_n00000128_mr10.0_mi30.0_prcircle_gfs0.0000E+00_gfd0.0000E+00_gfe0.0000E+00_nrm0_hlf0_bf0_ext00_M0128_MPI_space01_time128/../../../"
 cd "$SWEETROOT"
 
 module load mkl
