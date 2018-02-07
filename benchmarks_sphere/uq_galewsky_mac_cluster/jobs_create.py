fc610ad16ed6724837e52d472486d0f43c0bf74d
Martin Schreiber <M.Schreiber@exeter.ac.uk>
2018-02-07 15:42:51 -0800

    updated UQ script

--- benchmarks_sphere/uq_galewsky_mac_cluster/jobs_create.py
+++ benchmarks_sphere/uq_galewsky_mac_cluster/jobs_create.py
@@ -287,11 +287,13 @@ p.runtime.viscosity = 0.0
 #timestep_sizes = [timestep_size_reference*(2.0**i) for i in range(0, 11)]
 #timestep_sizes = [timestep_size_reference*(2**i) for i in range(2, 4)]
 
+
 timestep_sizes_explicit = [10, 20, 30, 60, 120, 180]
 timestep_sizes_implicit = [60, 120, 180, 360, 480, 600, 720]
 timestep_sizes_rexi = [60, 120, 180, 240, 300, 360, 480, 600, 720]
 
-timestep_size_reference = timestep_sizes_explicit[0]
+
+timestep_size_reference = 10
 
 #timestep_sizes = timestep_sizes[1:]
 #print(timestep_sizes)
@@ -331,250 +333,147 @@ groups = ['ln2']
 #
 if __name__ == "__main__":
 
-	####################################################
-	# WE FOCUS ON 2ND ORDER ACCURATE METHODS HERE
-	####################################################
-	groups = ['ln2']
-
-
-	if len(sys.argv) > 1:
-		groups = [sys.argv[1]]
-
-	print("Groups: "+str(groups))
-
-	for group in groups:
-		# 1st order linear
-
-		# 2nd order nonlinear
-		if group == 'ln2':
-			ts_methods = [
-				['ln_erk',		4,	4,	0],	# reference solution
-
-				###########
-				# RK2/4
-				###########
-				#['ln_erk',		2,	2,	0],	# reference solution
-				#['ln_erk',		4,	4,	0],	# reference solution
-
-				###########
-				# CN
-				###########
-				#['lg_irk_lc_n_erk_ver0',	2,	2,	0],
-				['lg_irk_lc_n_erk_ver1',	2,	2,	0],
-
-				#['l_irk_n_erk_ver0',	2,	2,	0],
-				#['l_irk_n_erk_ver1',	2,	2,	0],
-
-				###########
-				# REXI
-				###########
-				#['lg_rexi_lc_n_erk_ver0',	2,	2,	0],
-				#['lg_rexi_lc_n_erk_ver1',	2,	2,	0],
-
-				#['l_rexi_n_erk_ver0',	2,	2,	0],
-				#['l_rexi_n_erk_ver1',	2,	2,	0],
-
-				###########
-				# ETDRK
-				###########
-				#['lg_rexi_lc_n_etdrk',	2,	2,	0],
-				#['l_rexi_n_etdrk',	2,	2,	0],
-			]
-
-		# 4th order nonlinear
-		#if group == 'ln4':
-		#	ts_methods = [
-		#		['ln_erk',		4,	4,	0],	# reference solution
-		#		['l_rexi_n_etdrk',	4,	4,	0],
-		#		['ln_erk',		4,	4,	0],
-		#	]
-
-
+	ts_methods = [
+		['ln_erk',		4,	4,	0],	# reference solution
 
 		#
-		# OVERRIDE TS methods
+		# Choose implicit TS method for linear parts
+		# with explicit RK for non-linear parts
 		#
-		if len(sys.argv) > 4:
-			ts_methods = [ts_methods[0]]+[[sys.argv[2], int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5])]]
+		['lg_irk_lc_n_erk_ver1',	2,	2,	0],
+	]
 
 
+	#
+	# Reference solution
+	#
+	if True:
+	#if False:
+		tsm = ts_methods[0]
+		p.runtime.timestep_size  = timestep_size_reference
 
-		#
-		# add prefix string to group benchmarks
-		#
-		prefix_string_template = group
+		p.runtime.timestepping_method = tsm[0]
+		p.runtime.timestepping_order = tsm[1]
+		p.runtime.timestepping_order2 = tsm[2]
+		p.runtime.rexi_use_direct_solution = tsm[3]
 
+		p.cluster.par_time_cores = 1
 
+		if len(tsm) > 4:
+			s = tsm[4]
+			p.load_from_dict(tsm[4])
 
-		#
-		# Parallelization models
-		#
-		# Use 18 cores for each MPI task even if only 1 thread is used
-		# This avoid any bandwidth-related issues
-		#
-		# Parallelization model (18 threads per rank)
-#		p.cluster.pm_space_cores_per_mpi_rank = 18
-#		p.cluster.pm_time_cores_per_mpi_rank = 1
+		p.gen_script('script_ref'+p.runtime.getUniqueID(p.compile), 'run.sh')
 
-
-		#
-		# Reference solution
-		#
-		if True:
-		#if False:
-			tsm = ts_methods[0]
-			p.runtime.timestep_size  = timestep_size_reference
-
-			p.runtime.timestepping_method = tsm[0]
-			p.runtime.timestepping_order = tsm[1]
-			p.runtime.timestepping_order2 = tsm[2]
-			p.runtime.rexi_use_direct_solution = tsm[3]
-
-			p.cluster.par_time_cores = 1
-
-			if len(tsm) > 4:
-				s = tsm[4]
-				p.load_from_dict(tsm[4])
-
-			p.gen_script('script_'+prefix_string_template+'_ref'+p.runtime.getUniqueID(p.compile), 'run.sh')
+		ts_methods = ts_methods[1:]
 
 
+	#
+	# Create job scripts
+	#
+	for tsm in ts_methods:
+		tsm_name = tsm[0]
+		if 'ln_erk' in tsm_name:
+			timestep_sizes = timestep_sizes_explicit
+		elif 'l_erk' in tsm_name or 'lg_erk' in tsm_name:
+			timestep_sizes = timestep_sizes_explicit
+		elif 'l_irk' in tsm_name or 'lg_irk' in tsm_name:
+			timestep_sizes = timestep_sizes_implicit
+		elif 'l_rexi' in tsm_name or 'lg_rexi' in tsm_name:
+			timestep_sizes = timestep_sizes_rexi
+		else:
+			print("Unable to identify time stepping method "+tsm_name)
+			sys.exit(1)
 
-		#
-		# Create job scripts
-		#
-		for tsm in ts_methods[1:]:
-			tsm_name = tsm[0]
-			if 'ln_erk' in tsm_name:
-				timestep_sizes = timestep_sizes_explicit
-			elif 'l_erk' in tsm_name or 'lg_erk' in tsm_name:
-				timestep_sizes = timestep_sizes_explicit
-			elif 'l_irk' in tsm_name or 'lg_irk' in tsm_name:
-				timestep_sizes = timestep_sizes_implicit
-			elif 'l_rexi' in tsm_name or 'lg_rexi' in tsm_name:
-				timestep_sizes = timestep_sizes_rexi
+		p.runtime.timestepping_method = tsm[0]
+		p.runtime.timestepping_order = tsm[1]
+		p.runtime.timestepping_order2 = tsm[2]
+		p.runtime.rexi_use_direct_solution = tsm[3]
+
+		if len(tsm) > 4:
+			s = tsm[4]
+			p.runtime.load_from_dict(tsm[4])
+
+		print("Number of parameter samples: "+str(len(all_sg_points)))
+
+		for t in all_sg_points:
+			p.runtime.f	 	= t[0]
+			p.runtime.g 		= t[1]
+			p.runtime.r 		= t[2]
+			p.runtime.viscosity 	= t[3]
+			p.runtime.mode_res 	= int(t[4])
+			p.runtime.timestep_size 	= t[5]
+
+			p.runtime.benchmark_galewsky_umax = t[6]
+			p.runtime.benchmark_galewsky_hamp = t[7]
+			p.runtime.benchmark_galewsky_phi2 = t[8]
+
+			if not '_rexi' in p.runtime.timestepping_method:
+
+				p.runtime.rexi_method = ''
+				p.cluster.par_time_cores = 1
+				p.gen_script('script'+p.runtime.getUniqueID(p.compile)+'_'+p.cluster.getUniqueID(), 'run.sh')
+
 			else:
-				print("Unable to identify time stepping method "+tsm_name)
-				sys.exit(1)
 
-			for (	
-					p.runtime.f,
-					p.runtime.g,
-					p.runtime.h,
-					#p.runtime.r,
-					#p.runtime.viscosity,
-					#p.runtime.mode_res,
-					#p.runtime.timestep_size,
+				# NOT YET USED, That's for Exponential Integrators
+				c = 1
+				#if True:
+				if False:
+					range_cores_single_socket = [1, 2, 4, 8, 12, 16, 18]
+					range_cores_node = range_cores_single_socket + [18+i for i in range_cores_single_socket]
+				else:
+					#range_cores_node = [18,36]
+					range_cores_node = [18]
 
-					p.runtime.benchmark_galewsky_umax,
-					p.runtime.benchmark_galewsky_hamp,
-					p.runtime.benchmark_galewsky_phi2
-				) in itertools.product(
-					samples_coriolis_omega,
-					samples_gravitation,
-					samples_h0,
-					#samples_earth_radius,
-					#samples_viscosity,
-					#samples_mode_res,
-					#samples_timestep_size,
+				if True:
+					#for N in [64, 128]:
+					#for N in [128, 256]:
+					#for N in [128, 256]:
+					for N in [128]:
 
-					samples_galewsky_umax,
-					samples_galewsky_hamp,
-					samples_galewsky_phi2
-				):
-					print(p.runtime.f)
-					print(p.runtime.g)
-					print(p.runtime.h)
-					print(p.runtime.r)
-					print(p.runtime.viscosity)
-					print(p.runtime.mode_res)
-					print(p.runtime.timestep_size)
+						range_cores = range_cores_node + [36*i for i in range(2, p.cluster.total_max_nodes)]
 
-					print(p.runtime.benchmark_galewsky_umax)
-					print(p.runtime.benchmark_galewsky_hamp)
-					print(p.runtime.benchmark_galewsky_phi2)
+						if p.runtime.rexi_ci_n not in range_cores:
+							range_cores.append(N)
+						range_cores.sort()
 
+						#for r in [25, 50, 75]:
+						# Everything starting and above 40 results in significant errors
+						#for r in [30, 50]:
+						#for r in [30, 60]:
+						for r in [30]:
+							#for gf in [0.01, 0.005, 0.001, 0.0005, 0.0001, 0.0]:
+							#for gf in [0.01, 0.005, 0.001, 0.0005, 0.0001, 0.0]:
+							#for gf in [1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001]:
+							#for gf_exp_N in [2, 4, 6, 10, 20, 40]:
+							#for gf_exp_N in [2, 4, 10]:
+							for gf_exp_N in [0]:
+								#for gf_scale in [0, 5, 10, 20, 50]:
+								for gf_scale in [0]:
 
-					p.runtime.timestepping_method = tsm[0]
-					p.runtime.timestepping_order = tsm[1]
-					p.runtime.timestepping_order2 = tsm[2]
-					p.runtime.rexi_use_direct_solution = tsm[3]
+									#for ci_max_real in [10, 5]:
+									for ci_max_real in [10.0]:
+										p.runtime.load_from_dict({
+											'rexi_method': 'ci',
+											'ci_n':N,
+											'ci_max_real':ci_max_real,
+											'ci_max_imag':r,
+											'half_poles':0,
+											'ci_gaussian_filter_scale':gf_scale,
+											#'ci_gaussian_filter_dt_norm':130.0,	# unit scaling for T128 resolution
+											'ci_gaussian_filter_dt_norm':0.0,	# unit scaling for T128 resolution
+											'ci_gaussian_filter_exp_N':gf_exp_N,
+										})
 
-					if len(tsm) > 4:
-						s = tsm[4]
-						p.runtime.load_from_dict(tsm[4])
-
-
-					if not '_rexi' in p.runtime.timestepping_method:
-
-						p.runtime.rexi_method = ''
-						p.cluster.par_time_cores = 1
-						p.gen_script('script_'+prefix_string_template+p.runtime.getUniqueID(p.compile)+'_'+p.cluster.getUniqueID(), 'run.sh')
-
-					else:
-
-						# NOT YET USED, That's for Exponential Integrators
-
-						c = 1
-						#if True:
-						if False:
-							range_cores_single_socket = [1, 2, 4, 8, 12, 16, 18]
-							range_cores_node = range_cores_single_socket + [18+i for i in range_cores_single_socket]
-						else:
-							#range_cores_node = [18,36]
-							range_cores_node = [18]
-
-						if True:
-							#for N in [64, 128]:
-							#for N in [128, 256]:
-							#for N in [128, 256]:
-							for N in [128]:
-
-								range_cores = range_cores_node + [36*i for i in range(2, p.cluster.total_max_nodes)]
-
-								if p.runtime.rexi_ci_n not in range_cores:
-									range_cores.append(N)
-								range_cores.sort()
-
-								#for r in [25, 50, 75]:
-								# Everything starting and above 40 results in significant errors
-								#for r in [30, 50]:
-								#for r in [30, 60]:
-								for r in [30]:
-									#for gf in [0.01, 0.005, 0.001, 0.0005, 0.0001, 0.0]:
-									#for gf in [0.01, 0.005, 0.001, 0.0005, 0.0001, 0.0]:
-									#for gf in [1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001]:
-									#for gf_exp_N in [2, 4, 6, 10, 20, 40]:
-									#for gf_exp_N in [2, 4, 10]:
-									for gf_exp_N in [0]:
-										#for gf_scale in [0, 5, 10, 20, 50]:
-										for gf_scale in [0]:
-
-											#for ci_max_real in [10, 5]:
-											for ci_max_real in [10.0]:
-												p.runtime.load_from_dict({
-													'rexi_method': 'ci',
-													'ci_n':N,
-													'ci_max_real':ci_max_real,
-													'ci_max_imag':r,
-													'half_poles':0,
-													'ci_gaussian_filter_scale':gf_scale,
-													#'ci_gaussian_filter_dt_norm':130.0,	# unit scaling for T128 resolution
-													'ci_gaussian_filter_dt_norm':0.0,	# unit scaling for T128 resolution
-													'ci_gaussian_filter_exp_N':gf_exp_N,
-												})
-
-												#print(range_cores)
-												#sys.exit(1)
-												range_cores = [1]
-												for p.cluster.par_time_cores in range_cores:
-													#if p.cluster.par_time_cores >= p.runtime.rexi_ci_n:
-													if True:
-														# Generate only scripts with max number of cores
-														p.gen_script('script_'+prefix_string_template+p.runtime.getUniqueID(p.compile)+'_'+p.cluster.getUniqueID(), 'run.sh')
-														break
-
-	#					for p.cluster.par_time_cores in range_cores:
-	#						p.gen_script('script_'+prefix_string_template+p.runtime.getUniqueID(p.compile)+'_'+p.cluster.getUniqueID(), 'run.sh')
+										#print(range_cores)
+										#sys.exit(1)
+										range_cores = [1]
+										for p.cluster.par_time_cores in range_cores:
+											#if p.cluster.par_time_cores >= p.runtime.rexi_ci_n:
+											if True:
+												# Generate only scripts with max number of cores
+												p.gen_script('script'+p.runtime.getUniqueID(p.compile)+'_'+p.cluster.getUniqueID(), 'run.sh')
+												break
 
 
