307982b0ef0c96c93c0c6b1a7f213f6cce32f082
Pedro Peixoto <pedrosp@ime.usp.br>
2018-02-26 16:30:46 -0300

    script to compare different resolutions via interpolation

--- scripts/pp_plot_plane_unstablejet_compare.py
+++ scripts/pp_plot_plane_unstablejet_compare.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
 # Plot unstable jet fields
+#Campare against reference solutions
 # 
 #--------------------------------------
 
@@ -11,6 +12,16 @@ import numpy as np
 import sys
 import subprocess
 import os
+import math
+import scipy
+from scipy.interpolate import RectBivariateSpline
+import matplotlib.colors as colors
+
+
+#SWeet stuff
+from SWEETParameters import *
+
+#Go to working directory (need because .py is used as link)
 retval = os.getcwd()
 print ("Current working directory %s" % retval)
 os.chdir( retval )
@@ -20,7 +31,13 @@ os.chdir( retval )
 fontsize=12
 figsize=(9, 7)
 
+#Arguments check
+if len(sys.argv) != 3 :
+	print("I need 2 arguments:")
+	print("First file is reference file, second file is the analysed file")
+	sys.exit(1)
 
+#Get data files
 i=0
 for filename in sys.argv[1:]:
 	i = i + 1
@@ -31,19 +48,102 @@ for filename in sys.argv[1:]:
 	else:
 		filename2 = filename
 		data2 = np.loadtxt(filename) 
+		
+#Check dimensions
+data_ref = data1
+data_cmp = data2
+(ny_ref, nx_ref) = data_ref.shape
+(ny_cmp, nx_cmp) = data_cmp.shape
+print("Dimensions (ref and cmp)")
+print(ny_ref, nx_ref)
+print(ny_cmp, nx_cmp)
+
+#Set benchmark 
+earth = EarthMKSDimensions()
+benchpar = Unstablejet() 
+
+#Domain
+x_min = benchpar.x_min
+x_max = benchpar.x_max
+y_min = benchpar.y_min
+y_max = benchpar.y_max
+
+multiplier_j = (ny_ref)/(ny_cmp)
+multiplier_i = (nx_ref)/(nx_cmp)
+
+#if not float(multiplier_i).is_integer() or not float(multiplier_j).is_integer() : 
+#	print ("Dimensions of reference solution: ", ny_ref, nx_ref)
+#	print ("Dimensions of method under analysis: ", ny_cmp, nx_cmp)
+#	print ("Multipliers: ", multiplier_i, multiplier_j)
+#	print ("Grids are not aligned")
+#	sys.exit(1)
 	
-data = data1 - data2
+multiplier_j = int(multiplier_j)
+multiplier_i = int(multiplier_i)
+#print("Grids aligned")
+#print ("Multipliers (int): ", multiplier_i, multiplier_j)
+
+norm_l1_value = 0.0
+norm_l2_value = 0.0
+norm_linf_value = 0.0
+
+if multiplier_i == 1 and multiplier_j == 1: #Grids are the same
+	data = data_cmp - data_ref
+elif multiplier_i >= 1 and multiplier_j >= 1 :
+	if False:
+		print("Pointwise matching - Constains a bug!!!")
+		sys.exit(1)
+		#Data stores the difference and has the shape of cmp data (smaller)
+		data=data_cmp
+
+		for j in range(0, ny_cmp):
+			for i in range(0, nx_cmp):
+				#print("(",i,",",j,",", i*multiplier_i,",", j*multiplier_j,")", end="")
+
+				data[j,i] = data_cmp[j,i]-data_ref[j*multiplier_j,i*multiplier_i]
+		
+	else: 
+	#Comparison via interpolation
+		print("Interpolation")
+		# A-grid REFERENCE (file1) - sweet outputs only A grids physical space
+		dx_ref=(x_max-x_min)/(nx_ref)
+		dy_ref=(y_max-y_min)/(ny_ref)
+		x_ref = np.arange(x_min, x_max, dx_ref)
+		y_ref = np.arange(y_min, y_max, dy_ref)
+		x_ref += dx_ref/2
+		y_ref += dy_ref/2
+		X_ref, Y_ref = np.meshgrid(x_ref, y_ref)
+
+		#Creat cubic interpolation of reference file
+		interp_spline = RectBivariateSpline(y_ref, x_ref, data_ref)
+
+		#A-grid cmp file (file2)
+		dx_cmp=(x_max-x_min)/nx_cmp
+		dy_cmp=(y_max-y_min)/ny_cmp
+		x_cmp = np.arange(x_min, x_max, dx_cmp)
+		y_cmp = np.arange(y_min, y_max, dy_cmp)
+		x_cmp += dx_cmp/2
+		y_cmp += dy_cmp/2
+		X_cmp, Y_cmp = np.meshgrid(x_cmp, y_cmp)
+
+		#Get reduced reference resolution 
+		data_ref_low = interp_spline(y_cmp, x_cmp)
+		data = data_cmp - data_ref_low 
+		
+else :
+	print ("Please provide reference solution (file1) with dimension larger or equal file2")
+	sys.exit(1)
 
 
-#Get max/min
-cmin = np.amin(data)
-cmax = np.amax(data)
+#norm_l1_value = data
+norm_l1_value = np.sum(np.absolute(data))
+norm_l1_value = norm_l1_value/(nx_cmp*ny_cmp)
+norm_l2_value = np.sum(np.square(data))
+norm_l2_value = math.sqrt(norm_l2_value/(nx_cmp*ny_cmp))
+norm_linf_value = np.max(np.absolute(data))
+
 
 #Set physical grid for axis
-x_min = 0
-x_max = 4.00316e+07/1000/1000
-y_min = 0
-y_max = 4.00316e+07/1000/1000
 n = data.shape[0]
 x = np.linspace(x_min, x_max, n)
 y = np.linspace(y_min, y_max, n)
@@ -55,22 +155,43 @@ labelsy = np.linspace(y_min, y_max, 7)
 #Start plotting
 plt.figure(figsize=figsize)
 
-#Contour levels for fields
-s = 2e-5
-eta_contour_levels = np.append(np.arange(-1e-4, 0, s), np.arange(s, 1e-4, s))
-hs = 5
-h_contour_levels = np.append(np.arange(900, 1000-hs, hs), np.arange(1000+hs, 1100, hs))
+# set the colormap and centre the colorbar
+class MidpointNormalize(colors.Normalize):
+	"""
+	Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
 
-extent = (labelsx[0], labelsx[-1], labelsy[0], labelsy[-1])
+	e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+	"""
+	def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+		self.midpoint = midpoint
+		colors.Normalize.__init__(self, vmin, vmax, clip)
+
+	def __call__(self, value, clip=None):
+		# I'm ignoring masked values and all kinds of edge cases to make a
+		# simple example...
+		x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+		return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
+		
+
+#Get max/min
+cmin = np.amin(data)
+cmax = np.amax(data)		
+mid_val=0		
+		
+extent = (labelsx[0]+dx_cmp/2, labelsx[-1]-dx_cmp/2, labelsy[0]+dy_cmp/2, labelsy[-1]-dy_cmp/2)
 
 #Color plot
 #plt.imshow(data, interpolation='nearest', extent=extent, origin='lower', aspect='auto')
 #plt.imshow(data, interpolation='nearest', origin='lower', aspect='auto', cmap=plt.get_cmap('seismic'))
 #plt.imshow(data, interpolation='nearest', origin='lower')
-plt.imshow(data, interpolation='nearest', extent=extent, origin='lower', aspect='auto', cmap=plt.get_cmap('seismic'))
+plt.imshow(data, interpolation='nearest', extent=extent, origin='lower', aspect='auto', cmap=plt.get_cmap('seismic'), norm=MidpointNormalize(midpoint=mid_val,vmin=cmin, vmax=cmax))
+#plt.imshow(ras, cmap=cmap, clim=(elev_min, elev_max), norm=MidpointNormalize(midpoint=mid_val,vmin=elev_min, vmax=elev_max))
+#plt.pcolor(x_cmp, y_cmp, data)
 
-#Colorbar
+#Colorbar - centred
 plt.clim(cmin, cmax)
+cref=max(abs(cmin),abs(cmax))
+plt.clim(-cref, +cref)
 if 'diag_vort' in filename:
 	cbar = plt.colorbar(format='%.0e')
 else:
@@ -79,12 +200,18 @@ cbar.ax.tick_params(labelsize=fontsize)
 
 #Contour lines (black)
 if 'diag_vort' in filename:
-#		plt.contour(data, colors="black", origin='lower', extent=extent, vmin=cmin, vmax=cmax, levels=eta_contour_levels, linewidths=0.5)
+	s = 2e-5
+	eta_contour_levels = np.append(np.arange(-1e-4, 0, s), np.arange(s, 1e-4, s))
+
+	#plt.contour(data, colors="black", origin='lower', extent=extent, vmin=cmin, vmax=cmax, levels=eta_contour_levels, linewidths=0.5)
 	plt.contour(x,y,data, colors="black", origin='lower', vmin=cmin, vmax=cmax, levels=eta_contour_levels, linewidths=0.5)
 	#plt.contourf(x, y, data, vmin=cmin, vmax=cmax, levels=eta_contour_levels)
 elif 'prog_h' in filename:
 	#plt.contour(data, colors="black", origin='lower', extent=extent, vmin=cmin, vmax=cmax, levels=h_contour_levels, linewidths=0.5)
-	plt.contour(x,y, data, colors="black", origin='lower', vmin=cmin, vmax=cmax, levels=h_contour_levels, linewidths=0.5)
+	hs = 2
+	#h_contour_levels = np.append(np.arange(900, 1000-hs, hs), np.arange(1000+hs, 1100, hs))
+	h_contour_levels = np.append(np.arange(-2, 0, hs), np.arange(0, 2, hs))
+	#plt.contour(x,y, data, colors="black", origin='lower', vmin=cmin, vmax=cmax, levels=h_contour_levels, linewidths=0.5)
 else:
 	if cmin != cmax:
 		pass
@@ -94,14 +221,14 @@ else:
 title=""
 outfile=""
 if 'diag_vort' in filename:
-	title+="Vorticity Deviation "
-	outfile+="vort"
+	title+="Vorticity Deviation \n"
+	outfile+="vort_"
 elif 'prog_h' in filename:
-	title+="Depth Deviation (m) "
-	outfile+="depth"
+	title+="Depth Deviation (m) \n"
+	outfile+="depth_"
 else:
-	title+="What is this?"
-	outfile+="what"
+	title+="What is this? \n"
+	outfile+="what_"
 	
 #Method
 print("Methods")
@@ -122,6 +249,11 @@ elif method1 == "l_rexi_na_sl_nd_etdrk":
 	method1 = "SL-ETD2RK"
 elif method1 == "l_rexi_n_etdrk":
 	method1 = "ETD2RK"
+elif method1 == "ln_erk":
+	if 'ref' in filename1:
+		method1 = "REF"
+	else:
+		method1 = "RK-FDC"
 	
 if method2 == "l_cn_na_sl_nd_settls":
 	method2 = "SL-SI-SETTLS"
@@ -131,6 +263,11 @@ elif method2 == "l_rexi_na_sl_nd_etdrk":
 	method2 = "SL-ETD2RK"
 elif method2 == "l_rexi_n_etdrk":
 	method2 = "ETD2RK"
+elif method2 == "ln_erk":
+	if 'ref' in filename2:
+		method2 = "REF"
+	else:
+		method2 = "RK-FDC"
 		
 if method1 == method2:
 	title+=method1
@@ -206,12 +343,20 @@ plt.ylabel("y (1000 km)", fontsize=fontsize)
 
 plt.show()
 
+#plt.close()
+
 #Save file as eps
 #outfilename = filename.replace('.csv', 'compare.eps')
 outfilename=outfile+".eps"
 print(outfilename)
 plt.savefig(outfilename, dpi=300)
-#plt.show()
-plt.close()
+
+
+#
+# L1, L2, Linf
+outfile_errors = outfilename.replace('.eps', 'Errors12max.txt')
+print(outfile_errors+"\t"+str(norm_l1_value)+"\t"+str(norm_l2_value)+"\t"+str(norm_linf_value))
+
+print(str(norm_l1_value)+"\t"+str(norm_l2_value)+"\t"+str(norm_linf_value), file=open(outfile_errors, 'w'))
 
 
