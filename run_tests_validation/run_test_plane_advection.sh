49a102c74ab63589d52f61ac46ebb668eb52d106
Martin Schreiber <schreiberx@gmail.com>
2018-04-19 02:14:57 +0200

    removed time consuming 1st order test for na_erk

--- run_tests_validation/run_test_plane_advection.sh
+++ run_tests_validation/run_test_plane_advection.sh
@@ -22,6 +22,18 @@ $SCONS
 #./build/test_plane_advection_* -M 64 --dt=0.01 -X 2 -Y 2 --benchmark=gaussian_bump_advection --timestepping-method=na_erk --timestepping-order=4  --advection-velocity=0.1,0.2,1 -t 20
 
 
+#for r in 0 1 20; do
+for r in 1 20; do
+	for vu in 0.1 -0.2; do
+		for vv in -0.1 0.2; do
+			# order 1
+			EXEC="./build/test_plane_advection_* -M 128 --dt=0.05 -X 2 -Y 2 --benchmark=gaussian_bump_advection --timestepping-method=na_sl --timestepping-order=1  --advection-velocity=$vu,$vv,$r -t 20"
+			echo "$EXEC"
+			$EXEC || exit
+		done
+	done
+done
+
 
 # rotation speeds
 # 0: no rotation
@@ -32,17 +44,6 @@ for r in 0 1 20; do
 		for vv in -0.1 0.2; do
 			# order 2
 			EXEC="./build/test_plane_advection_* -M 64 --dt=0.1 -X 2 -Y 2 --benchmark=gaussian_bump_advection --timestepping-method=na_sl --timestepping-order=2  --advection-velocity=$vu,$vv,$r -t 20"
-			echo "$EXEC"
-			$EXEC || exit
-		done
-	done
-done
-
-for r in 0 1 20; do
-	for vu in 0.1 -0.2; do
-		for vv in -0.1 0.2; do
-			# order 1
-			EXEC="./build/test_plane_advection_* -M 128 --dt=0.1 -X 2 -Y 2 --benchmark=gaussian_bump_advection --timestepping-method=na_sl --timestepping-order=1  --advection-velocity=$vu,$vv,$r -t 20"
 			echo "$EXEC"
 			$EXEC || exit
 		done
@@ -64,9 +65,9 @@ for r in 0 20; do
 			$EXEC || exit
 
 			# order 1
-			EXEC="./build/test_plane_advection_* -M 64 --dt=0.002 -X 2 -Y 2 --benchmark=gaussian_bump_advection --timestepping-method=na_erk --timestepping-order=1  --advection-velocity=$vu,$vv,$r -t 20"
-			echo "$EXEC"
-			$EXEC || exit
+#			EXEC="./build/test_plane_advection_* -M 64 --dt=0.002 -X 2 -Y 2 --benchmark=gaussian_bump_advection --timestepping-method=na_erk --timestepping-order=1  --advection-velocity=$vu,$vv,$r -t 20"
+#			echo "$EXEC"
+#			$EXEC || exit
 
 		done
 	done
