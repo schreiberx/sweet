760f5d2fa4044522f6511f6ecd95db361eb491db
Pedro Peixoto <pedrosp@ime.usp.br>
2018-05-28 15:52:38 -0300

    clean script

--- benchmarks_plane/sl-rexi/postprocess_joinerrors.sh
+++ benchmarks_plane/sl-rexi/postprocess_joinerrors.sh
@@ -1,15 +1,1 @@
-#! /bin/bash
-
-
-THISDIR=`pwd`
-
-#Verify arguments
-files=depth*Errors12max.txt
-outfile=Errors12max.txt
-echo "method/dt Error1 Error2 ErrorMax" > $outfile
-for i in $files; do
-    echo "$i"
-    errors=`cat $i`
-    echo $i $errors >> $outfile
-done
-#cat $outfile
+../../scripts/postprocess_joinerrors.sh
