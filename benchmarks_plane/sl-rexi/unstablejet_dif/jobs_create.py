45f8651866b5715d78baaa32c0cd3866907fe94e
Pedro Peixoto <pedrosp@ime.usp.br>
2018-05-28 15:42:18 -0300

    difusion bench

--- benchmarks_plane/sl-rexi/unstablejet_dif/jobs_create.py
+++ benchmarks_plane/sl-rexi/unstablejet_dif/jobs_create.py
@@ -84,7 +84,7 @@ p.runtime.viscosity = 0.0
 p.runtime.viscosity_order = 2 #hyperviscosity
 p.runtime.uselocalvisc = 1
 visclevels = 10 #7 #5
-visc_reference = 10000 #3600 #1 hour  #864000/10 #1 day
+visc_reference = 100 #3600 #1 hour  #864000/10 #1 day
 visc_sizes = [visc_reference*(10**(i)) for i in range(0, visclevels)]
 
 
@@ -107,10 +107,10 @@ for group in groups:
 		ts_methods = [
 			['ln_erk',		4,	4],	# reference solution
 			#['ln_erk',		2,	2],	# FD- C-grid
-			#['l_cn_na_sl_nd_settls', 2,	2],	# SI-SL-SP
+			['l_cn_na_sl_nd_settls', 2,	2],	# SI-SL-SP
 			['l_rexi_na_sl_nd_settls',	2,	2], #SL-EXP-SETTLS
 			['l_rexi_na_sl_nd_etdrk',	2,	2], #SL-EXP-ETDRK
-			#['l_rexi_n_etdrk',	2,	2], #ETDRK2
+			['l_rexi_n_etdrk',	2,	2], #ETDRK2
 			#['l_rexi_n_erk',	2,	2], #strang split
 		]
 
