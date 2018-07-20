ac892c897082cd42b948d92862312d26c21ec941
Pedro <pedrosp@ime.usp.br>
2018-07-19 22:56:40 -0300

    convert uv to ke spectrum

--- scripts/postprocess_uv2kespec.py
+++ scripts/postprocess_uv2kespec.py
@@ -43,37 +43,28 @@ if 'prog_v' not in vfile:
 	
 udata = np.loadtxt(ufile)
 vdata = np.loadtxt(vfile)
+print("Dimensions (u,v)")
+print(udata.shape, vdata.shape)
 
-plt.figure(1, figsize=figsize)
-plt.imshow(udata, interpolation='nearest', origin='lower', aspect='auto', cmap=plt.get_cmap('jet'))
-plt.savefig('udata.eps', dpi=300, transparent=True, bbox_inches='tight', pad_inches=0)
+uspec=np.fft.fft2(udata)/udata.shape[0]/udata.shape[1]
+vspec=np.fft.fft2(vdata)/vdata.shape[0]/vdata.shape[1]
 
-plt.figure(2, figsize=figsize)
-plt.imshow(vdata, interpolation='nearest', origin='lower', aspect='auto', cmap=plt.get_cmap('jet'))
-plt.savefig('vdata.eps', dpi=300, transparent=True, bbox_inches='tight', pad_inches=0)
+print("Spectral Dimensions (u,v)")
+print(uspec.shape, vspec.shape)
+
+#see https://journals.ametsoc.org/doi/10.1175/1520-0469%282001%29058<0329%3ATHKESA>2.0.CO%3B2
+data=0.25*(np.multiply(uspec,np.conjugate(uspec)))+0.25*(np.multiply(vspec,np.conjugate(vspec)))
+data=data.real
+
+n=data.shape[0]
+print(n)
+
+data=data[0:int(n/2)-1, 0:int(n/2)-1]
 
 
-print(udata)
-print(vdata)
-
-uspec=np.fft.fft2(udata)
-vspec=np.fft.fft2(vdata)
-
-plt.figure(3, figsize=figsize)
-plt.imshow(abs(uspec), interpolation='nearest', origin='lower', aspect='auto', cmap=plt.get_cmap('jet'))
-plt.savefig('uspec.eps', dpi=300, transparent=True, bbox_inches='tight', pad_inches=0)
-
-plt.figure(4, figsize=figsize)
-plt.imshow(abs(vspec), interpolation='nearest', origin='lower', aspect='auto', cmap=plt.get_cmap('jet'))
-plt.savefig('vspec.eps', dpi=300, transparent=True, bbox_inches='tight', pad_inches=0)
-
-print(abs(uspec)>1)
-print(vspec)
-
-sys.exit()
 #Get max/min
-cmin = np.amin(udata)
-cmax = np.amax(udata)
+cmin = np.amin(data)
+cmax = np.amax(data)
 
 earth = EarthMKSDimensions()
 benchpar = Unstablejet()
@@ -83,8 +74,6 @@ xL_min = benchpar.x_min
 xL_max = benchpar.x_max
 yL_min = benchpar.y_min
 yL_max = benchpar.y_max
-
-
 
 #Set physical grid for axis
 n = data.shape[1]
@@ -103,10 +92,10 @@ labelsx = np.linspace(x_min, x_max, 10)
 labelsy = np.linspace(y_min, y_max, 10)
 
 #Set tittle
-title=""	
-if 'ke' in filename:
-	title+="Kinetic Energy Spectrum "
-	
+
+title="Kinetic Energy Spectrum "
+
+filename=ufile
 #Method
 print("Methods")
 pos1 = filename.find('_tsm_')
@@ -165,7 +154,7 @@ plt.clim(cmin, cmax)
 cbar = plt.colorbar()	
 		
 if 'ke' in filename:	
-	cbar.set_label('Spectral density ($m^2s^{-2}$)', rotation=270, labelpad=+20, size=fontsize)
+	cbar.set_label('Kinetic Energy ($m^2s^{-2}$)', rotation=270, labelpad=+20, size=fontsize)
 	cbar.ax.tick_params(labelsize=fontsize) 
 
 
@@ -203,42 +192,23 @@ plt.figure(2, figsize=figsize)
 r=np.arange(0, m+1, 1) #radius
 energy=np.zeros(m+1)
 shell_pattern=np.zeros((m+1, m+1))
-if 0:
-	#Polar coordinates for shell spectrum
-	t=np.arange(0, np.pi/2, np.pi/2/m) #angle
-	
-	#Create cubic interpolation of reference file
-	interp_spline = RectBivariateSpline(x, y, data)
-	
-	i=0
-	for ri in r:
-		energy[i]=0.0
-		j=0
-		for tj in t:
-			x_tmp=ri*np.cos(tj)
-			y_tmp=ri*np.sin(tj)
-			energy[i] += interp_spline(x_tmp, y_tmp)/float(m)
-			#print(j, x_tmp, y_tmp, energy[i], data[0,0], m)
-			j=j+1
-		i=i+1
-else:
-	print("Generating energy in shells (Each x is 1/", m, ")")
-	for i in range(0,m):
-		for j in range(0,m):
-			k=np.sqrt(pow(float(i),2)+pow(float(j),2))
-			intk=int(k)
-			if intk < m :
-				energy[intk]=energy[intk]+data[i,j]
-				shell_pattern[i,j]=intk
-		print(".", end='', flush=True)
-		#print(i, j, k, intk, data[i,j], energy[intk], data.shape, energy.shape)
+print("Generating energy in shells (Each x is 1/", m, ")")
+for i in range(0,m):
+	for j in range(0,m):
+		k=np.sqrt(pow(float(i),2)+pow(float(j),2))
+		intk=int(k)
+		if intk < m :
+			energy[intk]=energy[intk]+data[i,j]
+			shell_pattern[i,j]=intk
+	print(".", end='', flush=True)
+	#print(i, j, k, intk, data[i,j], energy[intk], data.shape, energy.shape)
 			
 print(".")
 #Quick check to see if things match
 print("Energy in shells: ", energy[0:10])
 print("Energy in first column of data: ", data[0:10,0])
-print("Shell modes: ", r[0:10])
-print("Pattern:\n", shell_pattern[0:10,0:10])
+#print("Shell modes: ", r[0:10])
+#print("Pattern:\n", shell_pattern[0:10,0:10])
 
 
 
@@ -253,18 +223,24 @@ print("Pattern:\n", shell_pattern[0:10,0:10])
 #	en_ref53=np.append(en_ref53, [ytmp])
 	#print(en_ref53)
 
-r_ref3=r[-int(m/4):-1]
+r_ref3=r[-int(m/2):-1]
 
 offsetx=m*1000
 offsety=0.005
 en_ref3=np.array([])
+en_ref53=np.array([])
+i=r_ref3[0]
+iref=(energy[1]/10.0)/np.power(float(i), -3)
 for tmp in r_ref3:
-	ytmp=np.power(tmp, -float(5.0/3.0))*offsetx
-	#print(tmp, ytmp)
+	ytmp=np.power(tmp, -float(3.0))*iref
 	en_ref3=np.append(en_ref3, [ytmp])
-	#print(en_ref53)
 
-plt.title(title, fontsize=fontsize)
+iref=(energy[1]/10.0)/np.power(float(i), -float(5.0/3.0))	
+for tmp in r_ref3:
+	ytmp=np.power(tmp, -float(5.0/3.0))*iref
+	en_ref53=np.append(en_ref53, [ytmp])
+	
+plt.title(title, fontsize=fontsize, y=1.02)
 
 #Convert wavenumber to wavelength
 r=xL_max*1000/r
@@ -273,12 +249,14 @@ r_ref3=xL_max*1000/r_ref3
 plt.loglog(r, energy)
 #plt.loglog(r_ref53, en_ref53, '-.', color='black')
 plt.loglog(r_ref3, en_ref3, '-.', color='black')
-
+plt.loglog(r_ref3, en_ref53, '-.', color='black')
 
 #Axis
 ax = plt.gca()
-#ax.annotate("-5/3", xy=(r_ref53[50], en_ref53[50]+0.01), fontsize=fontsize)
-ax.annotate("$k^{-5/3}$", xy=(r_ref3[-int(m/8)], en_ref3[-int(m/8)]+0.001), fontsize=fontsize)
+ax.annotate("$k^{-5/3}$", xy=(r_ref3[-1]+5, en_ref53[-1]), fontsize=fontsize)
+ax.annotate("$k^{-3}$", xy=(r_ref3[-1]+5, en_ref3[-1]), fontsize=fontsize)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
 
 ax.xaxis.set_label_coords(0.5, -0.075)
 
@@ -293,11 +271,12 @@ plt.xlabel("Horizontal wavenumber", fontsize=fontsize)
 plt.xlabel("Horizontal wavelength (km)", fontsize=fontsize)
 
 #plt.yticks(labelsy, fontsize=fontsize)
-plt.ylabel("Spectral density $m^3s^{-2}$", fontsize=fontsize)
+plt.ylabel("Kinetic Energy $(m^2s^{-2})$", fontsize=fontsize)
 
 
 #Save file as eps
-outfilename = filename.replace('.csv', '_1D.eps')
+outfilename = filename.replace('prog_u', 'kenergyspec')
+outfilename = outfilename.replace('.csv', '_1D.eps')
 
 outfilename = outfilename.replace('/output', '')
 print(outfilename)
