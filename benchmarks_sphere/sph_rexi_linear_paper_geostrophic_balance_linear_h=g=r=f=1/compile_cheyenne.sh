4a48ec2498c9cc871dc15099fef760ca6e11a875
Martin Schreiber <SchreiberX@gmail.com>
2018-09-30 22:28:57 +0200

    benchmark_sphere cleanups

--- benchmarks_sphere/sph_rexi_linear_paper_geostrophic_balance_linear_h=g=r=f=1/compile_cheyenne.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-SWEETROOT="/glade/u/home/martins/workspace/sweet/benchmarks_sphere/sph_rexi_linear_paper_geostrophic_balance_linear_h=g=r=f=1/script_g1_h1_f1_a1_fsph0_u0_U0_tsm_l_erk_tso2_tsob1_C000.01_M0064/../../../"
-cd "$SWEETROOT"
-
-scons  --mode=release --compiler=intel --debug-symbols=enable --simd=enable --mic=disable --fortran-source=enable --program-binary-name= --sweet-mpi=disable --threading=off --rexi-thread-parallel-sum=enable --rexi-timings=disable --rexi-timings-additional-barriers=disable --numa-block-allocator=2 --program=swe_sphere --parareal=none --libpfasst=disable --pfasst-cpp=disable --eigen=disable --libfft=enable --libsph=enable --mkl=disable --plane-spectral-space=disable --plane-spectral-dealiasing=disable --sphere-spectral-space=enable --sphere-spectral-dealiasing=enable --libxml=disable --compiler-c-exec= --compiler-cpp-exec= --compiler-fortran-exec= --gui=disable --quadmath=disable
-
