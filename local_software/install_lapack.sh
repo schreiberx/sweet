36f95e805d08b5748b3ab23205ec4b7162c33b5f
Martin Schreiber <schreiberx@gmail.com>
2018-10-03 05:50:08 -0600

    updated lapack install script

--- local_software/install_lapack.sh
+++ local_software/install_lapack.sh
@@ -6,10 +6,10 @@ source ./env_vars.sh ""
 
 echo "*** LAPACK ***"
 if [ ! -e "$DST_DIR/lib/liblapack.a"  -o "$1" != "" ]; then
-	SRC_LINK="https://www.martin-schreiber.info/pub/sweet_local_software/lapack-3.6.1.tgz"
+	SRC_LINK="https://www.martin-schreiber.info/pub/sweet_local_software/lapack-3.8.0.tar.gz"
 	#SRC_LINK="https://www.netlib.org/lapack/lapack-3.6.1.tgz"
 	FILENAME="`basename $SRC_LINK`"
-	BASENAME="lapack-3.6.1"
+	BASENAME="lapack-3.8.0"
 
 	cd "$SRC_DIR"
 
@@ -18,14 +18,41 @@ if [ ! -e "$DST_DIR/lib/liblapack.a"  -o "$1" != "" ]; then
 	tar xzf "$FILENAME"
 	cd "$BASENAME"
 
-	# create default configuration
-	cp make.inc.example make.inc
-
-	#./configure --prefix="$DST_DIR" || exit 1
-
 	ulimit -s 100000 || echo "FAILED TO INCREASE ULIMIT & now ignoring this problem"
-	make blaslib || exit 1
-	make || exit 1
+	if true; then
+		# Use cmake
+		mkdir -p build || exit
+
+		cd build || exit
+		cmake ../ || exit
+		make
+	else
+		# create default configuration
+		cp make.inc.example make.inc
+
+		# Replace default Fortran compiler
+		if [ ! -z "$FC" ]; then
+			# Default C compiler
+			sed -i "s/^CC.*=.*/CC = ${CC}/" make.inc
+
+			# Default Fortran compiler
+			sed -i "s/^FORTRAN.*=.*/FORTRAN = ${FC}/" make.inc
+
+			# Default linker
+			sed -i "s/^LOADER.*=.*/LOADER = ${FC}/" make.inc
+
+			# Get rid of this option
+			sed -i "s/-frecursive//" make.inc
+
+			# Use INT_CPU_TIME timer
+			sed -i "s/^TIMER.*=.*/TIMER = INT_CPU_TIME/" make.inc
+
+			
+		fi
+
+		make blaslib || exit 1
+		make || exit 1
+	fi
 
 	#cd LAPACKE || exit 1
 	#make lapacke || exit 1
