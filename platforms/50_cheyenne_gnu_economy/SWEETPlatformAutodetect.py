e834e4e1377b1c84ae8d5a96442421d0d1ad2fef
Martin Schreiber <schreiberx@gmail.com>
2018-10-03 01:35:15 -0600

    autodetection on cheyenne

--- platforms/50_cheyenne_gnu_economy/SWEETPlatformAutodetect.py
+++ platforms/50_cheyenne_gnu_economy/SWEETPlatformAutodetect.py
@@ -11,17 +11,19 @@ def autodetect():
 		True if current platform matches, otherwise False
 	"""
 
+	fqdn = socket.getfqdn()
+	if not ".cheyenne" in fqdn:
+		return False
+
 	dirs = os.path.abspath(__file__).split('/')
 	sweet_src_dirname = dirs[len(dirs)-4]
+	print(sweet_src_dirname)
 
 	# Autodetect based on source folder name for SWEET source
 	# This helps to utilize different versions of SWEET on cheyenne
-	if sweet_src_dirname=="sweet_gnu_cheyenne":
+	if sweet_src_dirname=="sweet_gnu_economy":
 		return True
 
-	return False
-#	fqdn = socket.getfqdn()
-#	return ".cheyenne" in fqdn
 
 
 if __name__ == "__main__":
