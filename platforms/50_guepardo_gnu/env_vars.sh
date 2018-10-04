b68db0c6f4ae9e15aca10720e7881455f71972b3
Martin Schreiber <martin@guepardo00>
2018-10-04 11:19:04 -0300

    updated for panther

---
+++ platforms/50_guepardo_gnu/env_vars.sh
@@ -0,0 +1,33 @@
+
+#
+# hostname martinium
+#
+
+# No batch files
+export BATCH_FILE_TAG="-"
+
+
+#
+# SWEET Compiler environment
+#
+export SWEET_CC=gcc-6
+export SWEET_CXX=g++-6
+export SWEET_F90=gfortran-6
+
+export SWEET_MPICC=mpicc
+export SWEET_MPICXX=mpic++
+export SWEET_MPIF90=mpif90
+
+export SWEET_LINK=$SWEET_CXX
+export SWEET_MPILINK=$SWEET_MPICXX
+
+#
+# local software compile overrides
+#
+export F90=$SWEET_F90
+export FC=$F90
+export CC=$SWEET_CC
+export CXX=$SWEET_CXX
+export LINK=$SWEET_CXX
+export LD=ld
+
