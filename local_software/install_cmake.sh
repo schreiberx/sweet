ca2347ead3971e38de5b3a8c83db19051609f71e
Martin Schreiber <SchreiberX@gmail.com>
2018-10-04 14:38:42 +0200

    fix for cmake installer

--- local_software/install_cmake.sh
+++ local_software/install_cmake.sh
@@ -6,15 +6,10 @@ source ./install_helpers.sh "" || exit 1
 PKG_NAME="cmake"
 
 # Path to one file of installed package to test for existing installation
-PKG_INSTALLED_FILE="$SWEET_LOCAL_SOFTWARE_DST_DIR/bin/make"
+PKG_INSTALLED_FILE="$SWEET_LOCAL_SOFTWARE_DST_DIR/bin/cmake"
 
 # URL to source code to fetch it
 PKG_URL_SRC="cmake-3.12.3.tar.gz"
-
-if [ "`uname -s`" != "Linux" ] && [ "`uname -s`" != "Darwin" ]; then
-	echo "This script only supports make on Linux systems"
-	exit 1
-fi
 
 config_package $@
 
