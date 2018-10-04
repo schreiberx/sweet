38773cac0bfdf0fffc71d95651b553cb5d793b9f
Martin Schreiber <SchreiberX@gmail.com>
2018-10-04 16:18:40 +0200

    fixed typo in likwid installer script

--- platforms/80_martinium_gnu/local_software_default.sh
+++ platforms/80_martinium_gnu/local_software_default.sh
@@ -1,5 +1,4 @@
 
-
 SWEET_SYSTEM_PACKAGES="libxft-dev libssl-dev"
 if [ "x$DISPLAY" = "x:0" ]; then
 	export SWEET_SYSTEM_PACKAGES="$SWEET_SYSTEM_PACKAGES libgl1-mesa-dev"
@@ -14,25 +13,18 @@ for i in $SWEET_SYSTEM_PACKAGES; do
 	fi
 done
 
+#
+# Martin Schreiber's laptop (martinium)
+#
 
-#PKGS+=("install_autoconf.sh")
-#PKGS+=("install_automake.sh")
+#PKGS+=("install_gcc7.1.sh")
 PKGS+=("install_fftw3.sh")
-PKGS+=("install_eigen3.sh")
-#PKGS+=("install_gcc5.3.sh")
-PKGS+=("install_gcc7.2.sh")
+#PKGS+=("install_eigen3.sh")
 PKGS+=("install_lapack.sh")
-#PKGS+=("install_libfreetype.sh")
-#PKGS+=("install_libpfasst.sh")
-PKGS+=("install_libpng.sh")
-PKGS+=("install_likwid.sh")
-PKGS+=("install_numa.sh")
-PKGS+=("install_openmpi.sh")
-#PKGS+=("install_pfasst++.sh")
-PKGS+=("install_python3.sh")
-#PKGS+=("install_rdic.sh")
+#PKGS+=("install_openssl.sh")
+#PKGS+=("install_python3.sh")
 PKGS+=("install_scons3.sh")
-PKGS+=("install_sdl2.sh")
 PKGS+=("install_shtns.sh")
-
+PKGS+=("install_shtns_python.sh")
+#PKGS+=("install_numa.sh")
 
