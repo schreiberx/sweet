7e8173979462eb388726b3b9f44f1c972410a2dd
Martin Schreiber <SchreiberX@gmail.com>
2018-10-05 09:41:44 +0200

    added 'make test' to some install scripts

--- local_software/install_curl.sh
+++ local_software/install_curl.sh
@@ -2,18 +2,13 @@
 
 source ./install_helpers.sh "" || exit 1
 
-
-# Name of package
 PKG_NAME="curl"
-
-# Path to one file of installed package to test for existing installation
 PKG_INSTALLED_FILE="$SWEET_LOCAL_SOFTWARE_DST_DIR/bin/curl"
-
-# URL to source code to fetch it
 PKG_URL_SRC="curl-7.61.1.tar.xz"
 
 config_package $@
-
-config_configure_make_default_install
-
+config_configure_make_default
+config_exec "make test"
+config_configure_make_install
 config_success
+
