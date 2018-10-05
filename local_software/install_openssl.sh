0d2e720dbccf93c343de887f3a2c221c16c04a52
schreiberx <schreiberx@gmail.com>
2018-10-05 09:49:39 +0200

    fixed additional params for install_openssl script

--- local_software/install_openssl.sh
+++ local_software/install_openssl.sh
@@ -7,7 +7,7 @@ PKG_INSTALLED_FILE="$SWEET_LOCAL_SOFTWARE_DST_DIR/lib/libssl.so"
 PKG_URL_SRC="openssl-1.1.1.tar.gz"
 
 config_package $@
-config_exec "./config --prefix=$SWEET_LOCAL_SOFTWARE_DST_DIR $@"
+config_exec "./config --prefix=$SWEET_LOCAL_SOFTWARE_DST_DIR"
 config_make_default
 config_exec "make test"
 config_make_install
