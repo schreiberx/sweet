7ad12d372329f5a40f9f5cefde0b3a4204d59f88
Martin Schreiber <SchreiberX@gmail.com>
2018-10-05 20:31:52 +0200

    corrested libfreetype installed filename

--- local_software/install_libfreetype.sh
+++ local_software/install_libfreetype.sh
@@ -7,7 +7,7 @@ source ./install_helpers.sh "" || exit 1
 PKG_NAME="libfreetype"
 
 # Path to one file of installed package to test for existing installation
-PKG_INSTALLED_FILE="$SWEET_LOCAL_SOFTWARE_DST_DIR/lib/freetype6.so"
+PKG_INSTALLED_FILE="$SWEET_LOCAL_SOFTWARE_DST_DIR/lib/libfreetype.so"
 
 # URL to source code to fetch it
 PKG_URL_SRC="freetype-2.8.tar.gz"
