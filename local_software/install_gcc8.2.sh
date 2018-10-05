fa77c848a404a22167bfe7e83997f945cd684b9c
Martin Schreiber <martin@guepardo00>
2018-10-05 10:19:13 -0300

    updated SWEET_LOCAL_SOFTWARE_DST_DIR

--- local_software/install_gcc8.2.sh
+++ local_software/install_gcc8.2.sh
@@ -16,12 +16,12 @@ config_package $@
 echo_info "Downloading additional packages"
 ./contrib/download_prerequisites || echo_error_exit "Failed to download other required packages"
 
-config_configure --disable-multilib --enable-languages=c++,fortran  --prefix=$DST_DIR --program-suffix=-8.2
+config_configure --disable-multilib --enable-languages=c++,fortran  --prefix=$SWEET_LOCAL_SOFTWARE_DST_DIR --program-suffix=-8.2
 
 config_make_default_install
 
 for i in g++ gcc gcc-ar gcc-nm gcc-ranlib gfortran gcov gcov-tool gfortran; do
-	ln -sf "$DST_DIR/bin/$i-8.2" "$DST_DIR/bin/$i" || echo_error_exit "FAILED to create sym links"
+	ln -sf "$SWEET_LOCAL_SOFTWARE_DST_DIR/bin/$i-8.2" "$SWEET_LOCAL_SOFTWARE_DST_DIR/bin/$i" || echo_error_exit "FAILED to create sym links"
 done
 
 config_success
