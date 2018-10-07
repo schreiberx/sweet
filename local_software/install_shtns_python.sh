94b20f714e8d8fb0a3210f70b74d58c6a0b05ae2
Martin Schreiber <SchreiberX@gmail.com>
2018-10-07 23:58:38 +0200

    added workaround to shtns installer script

--- local_software/install_shtns_python.sh
+++ local_software/install_shtns_python.sh
@@ -17,7 +17,7 @@ config_package $@ || exit 1
 echo_info_hline
 echo_info "SHTNS Python noOpenMP:"
 # Python, no OpenMP
-config_configure --enable-python --disable-openmp || exit 1
+config_configure --disable-mem --enable-python --disable-openmp || exit 1
 config_make_clean || exit 1
 config_make_default || exit 1
 python3 setup.py install --prefix="$SWEET_LOCAL_SOFTWARE_DST_DIR" || echo_error_exit "Failed to install"
@@ -25,7 +25,7 @@ python3 setup.py install --prefix="$SWEET_LOCAL_SOFTWARE_DST_DIR" || echo_error_
 echo_info_hline
 echo_info "SHTNS Python OpenMP:"
 # Python, OpenMP
-config_configure --enable-python --enable-openmp || exit 1
+config_configure --disable-mem --enable-python --enable-openmp || exit 1
 config_make_clean || exit 1
 config_make_default || exit 1
 python3 setup.py install --prefix="$SWEET_LOCAL_SOFTWARE_DST_DIR" || echo_error_exit "Failed to install"
