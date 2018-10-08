eeb6d99890a6f6b865a562ed41572e48ee6ebac4
Martin Schreiber <schreiberx@gmail.com>
2018-10-07 21:16:43 -0300

    changed stopwatch

--- src/programs/swe_sphere/SWE_Sphere_TS_l_rexi.cpp
+++ src/programs/swe_sphere/SWE_Sphere_TS_l_rexi.cpp
@@ -971,8 +971,7 @@ void SWE_Sphere_TS_l_rexi::run_timestep(
 			}
 
 			#if SWEET_REXI_TIMINGS
-				if (stopwatch_measure)
-					SimulationBenchmarkTimings::getInstance().rexi_timestepping_reduce.stop()
+				SimulationBenchmarkTimings::getInstance().rexi_timestepping_reduce.stop();
 			#endif
 		}
 
