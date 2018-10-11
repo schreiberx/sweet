44bc9ab5d4b45b85e4d1a387d6ce917508f23f04
Martin Schreiber <schreiberx@gmail.com>
2018-10-11 18:01:34 -0300

    removed non-existing validation benchmark

--- run_tests_validation/run_test_swe_plane_timestepper_convergence_ln1.sh
+++ run_tests_validation/run_test_swe_plane_timestepper_convergence_ln1.sh
@@ -17,9 +17,9 @@ cd "run_test_swe_plane_timestepper_convergence"
 ./benchmark_create_job_scripts.py ln1 l_erk_n_erk 1 1 0 || exit 1
 ./benchmark_create_job_scripts.py ln1 l_irk_n_erk 1 1 0 || exit 1
 ./benchmark_create_job_scripts.py ln1 l_rexi_n_erk 1 1 1 || exit 1
-./benchmark_create_job_scripts.py ln1 lg_rexi_lc_n_erk 1 1 1 || exit 1
+#./benchmark_create_job_scripts.py ln1 lg_rexi_lc_n_erk 1 1 1 || exit 1
 ./benchmark_create_job_scripts.py ln1 l_rexi_n_etdrk 1 1 1 || exit 1
-./benchmark_create_job_scripts.py ln1 lg_rexi_lc_n_etdrk 1 1 1 || exit 1
+#./benchmark_create_job_scripts.py ln1 lg_rexi_lc_n_etdrk 1 1 1 || exit 1
 
 ./platform_jobs_run_directly || exit 1
 
