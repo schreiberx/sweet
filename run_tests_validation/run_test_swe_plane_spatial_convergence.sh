d67d9967b4fe88dcfe62a249eca23c2ee5d5dc71
Martin Schreiber <SchreiberX@gmail.com>
2018-10-11 18:23:48 +0200

    updated validation script for run_test_swe_plane_spatial_convergence

--- run_tests_validation/run_test_swe_plane_spatial_convergence.sh
+++ run_tests_validation/run_test_swe_plane_spatial_convergence.sh
@@ -5,51 +5,17 @@ echo "***********************************************"
 echo "Running convergence tests"
 echo "***********************************************"
 
-# set close affinity of threads
-export OMP_PROC_BIND=close
+cd "run_test_swe_plane_spatial_convergence"
 
+./cleanup_all || exit 1
 
-BASEDIR=`pwd`
+./benchmark_create_jobs || exit 1
 
-cd "$BASEDIR/run_test_swe_plane_spatial_convergence"
+./platform_jobs_run_directly || exit 1
 
-./cleanup.sh
+./postprocessing.py || exit 1
 
-
-# 1st order linear
-#./jobs_create.py l1 l_irk 1 0
-#./jobs_create.py l1 l_erk 1 0
-
-# 2nd order linear
-#./jobs_create.py l2 l_cn 2 0
-#./jobs_create.py l2 l_erk 2 0
-
-# 1st order nonlinear
-#./jobs_create.py ln1 ln_erk 1 1
-#./jobs_create.py ln1 l_erk_n_erk 1 1
-#./jobs_create.py ln1 l_irk_n_erk 1 1
-
-# 2nd order nonlinear
-#./jobs_create.py ln2 ln_erk 2 2
-#./jobs_create.py ln2 l_cn_n_erk 2 2
-#./jobs_create.py ln2 l_erk_n_erk 2 2
-
-# 2nd order nonlinear, SL-REXI related
-./jobs_create.py ln2space
-#./jobs_create.py ln2 ln_erk 2 2
-
-
-#./compile.sh || exit 1
-
-./jobs_run.sh || exit 1
-
-echo "***********************************************"
-echo " POSTPROCESSING "
-echo "***********************************************"
-./postprocessing_analytical.py || exit 1
-
-
-./cleanup.sh
+./cleanup_all || exit 1
 
 echo "***********************************************"
 echo "***************** FIN *************************"
