2bf5d7674a92b76af67ea75205ccd5a9733ca032
Martin Schreiber <SchreiberX@gmail.com>
2018-10-12 23:31:59 +0200

    updates

--- benchmarks_sphere/paper_jrn_parco_rexi_nonlinear/compare_wt_dt_vs_accuracy_galewsky_intel_32ranks/postprocessing_consolidate.py
+++ benchmarks_sphere/paper_jrn_parco_rexi_nonlinear/compare_wt_dt_vs_accuracy_galewsky_intel_32ranks/postprocessing_consolidate.py
@@ -16,6 +16,9 @@ groups = ['runtime.timestepping_method']
 
 tagnames_y = [
 	'sphere_data_diff_prog_h.norm_l1',
+#	'sphere_data_diff_prog_h.norm_l2',
+#	'sphere_data_diff_prog_h.norm_linf',
+#	'sphere_data_diff_prog_h.norm_rms',
 ]
 
 
@@ -34,14 +37,19 @@ def data_filter(x, y, jobdata):
 	x = float(x)
 	y = float(y)
 
+	if math.isnan(y):
+		return True
+
 	if 'timings' in tagname_x:
 		# Filter out NaNs for wallclock time studies
 		# NaNs require significantly more computation time
 		if math.isnan(y):
 			return True
-	# No filter!
-	# No filter!
-	# No filter!
+
+	# Filter out main timestepping loops which take over 10 minutes
+	if float(jobdata['output.simulation_benchmark_timings.main_timestepping']) > 60*10:
+		return True
+
 	return False
 
 	if y > 9.0:
@@ -55,6 +63,9 @@ def data_filter(x, y, jobdata):
 			return True
 
 	return False
+
+
+
 
 
 for tagname_y in tagnames_y:
@@ -92,51 +103,97 @@ for tagname_y in tagnames_y:
 		print("Processing tag "+tagname_x)
 		print("*"*80)
 
+
+		if False:
+			"""
+			Table format
+			"""
+
+			data_table = c.create_data_table_float(
+					groups,
+					tagname_x,
+					tagname_y,
+					data_filter = data_filter
+				)
+			fileid = "output_table_"+tagname_x.replace('.', '-').replace('_', '-')+"_vs_"+tagname_y.replace('.', '-').replace('_', '-')
+
+			print("Data table:")
+			c.print_data_table(data_table)
+			c.write_data_table(data_table, fileid+".csv")
+
+
+		if False:
+
+			"""
+			Plotting output
+			"""
+
+			data_plotting = c.create_data_plotting_float(
+					groups,
+					tagname_x,
+					tagname_y,
+					data_filter = data_filter
+				)
+
+			fileid = "output_plotting_"+tagname_x.replace('.', '-').replace('_', '-')+"_vs_"+tagname_y.replace('.', '-').replace('_', '-')
+
+			p = Plotting()
+			p.plot_scattered(
+					data_plotting = data_plotting,
+					xlabel = xlabel,
+					ylabel = ylabel,
+					title = title,
+					xscale = xscale,
+					yscale = yscale,
+					outfile=fileid+".pdf",
+				)
+
+			print("Data plotting:")
+			c.print_data_plotting(data_plotting)
+			c.write_data_plotting(data_plotting, fileid+".csv")
+
+
 		"""
-		Table format
+		Plotting with annotation
 		"""
 
-		data_table = c.create_data_table_float(
-				groups,
-				tagname_x,
-				tagname_y,
-				data_filter = data_filter
-			)
-		fileid = "output_table_"+tagname_x.replace('.', '-').replace('_', '-')+"_vs_"+tagname_y.replace('.', '-').replace('_', '-')
+		if True:
 
-		print("Data table:")
-		c.print_data_table(data_table)
-		c.write_data_table(data_table, fileid+".csv")
+			"""
+			Plotting output
+			"""
+
+			data_plotting = c.create_data_plotting_float(
+					groups,
+					tagname_x,
+					tagname_y,
+					data_filter = data_filter
+				)
+
+			fileid = "output_plotting_annotated_"+tagname_x.replace('.', '-').replace('_', '-')+"_vs_"+tagname_y.replace('.', '-').replace('_', '-')
+
+			p = Plotting()
+
+			p.plot_start()
+			p.plot_scattered_data_annotated(
+					data_plotting = data_plotting,
+					xlabel = xlabel,
+					ylabel = ylabel,
+					title = title,
+					xscale = xscale,
+					yscale = yscale,
+#					outfile=fileid+".pdf",
+#					render_each_nth_value
+				)
+			p.plot_finish(outfile=fileid+".pdf")
+
+			print("Data plotting:")
+			c.print_data_plotting(data_plotting)
+			c.write_data_plotting(data_plotting, fileid+".csv")
 
 
-		"""
-		Plotting format
-		"""
+print("Info:")
+print("	NaN: Errors in simulations")
+print("	None: No data available")
 
-		data_plotting = c.create_data_plotting_float(
-				groups,
-				tagname_x,
-				tagname_y,
-				data_filter = data_filter
-			)
 
-		fileid = "output_plotting_"+tagname_x.replace('.', '-').replace('_', '-')+"_vs_"+tagname_y.replace('.', '-').replace('_', '-')
-
-		p = Plotting()
-		p.plot_scattered(
-				data_plotting = data_plotting,
-				xlabel = xlabel,
-				ylabel = ylabel,
-				title = title,
-				xscale = xscale,
-				yscale = yscale,
-				outfile=fileid+".pdf",
-			)
-
-		print("Data plotting:")
-		c.print_data_plotting(data_plotting)
-		c.write_data_plotting(data_plotting, fileid+".csv")
-
-		print("Info:")
-		print("	NaN: Errors in simulations")
-		print("	None: No data available")
