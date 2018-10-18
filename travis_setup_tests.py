c277d5de3d8b26f8cc031b79fa964efa9144d3fa
Martin Schreiber <SchreiberX@gmail.com>
2018-10-18 19:24:54 +0200

    first travis test scripts

---
+++ travis_setup_tests.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+import glob
+import re
+
+travis_file=".travis.yml"
+
+print("Setting up tests in travis file '"+travis_file+"'")
+
+
+files = glob.glob('./tests/??_*/test.sh')
+
+tests = ""
+
+for f in files:
+	print(" + Found test script "+f)
+	tests += '  - '+f+"\n"
+
+
+with open(travis_file, 'r') as f:
+	travis_content = f.read()
+
+travis_content = re.sub(r'(###SCRIPT_START###).*(###SCRIPT_END###)', "\\1\nscript:\n"+tests+"\\2", travis_content, flags=re.MULTILINE | re.DOTALL)
+
+print("Writing content to file '"+travis_file+"'")
+with open(travis_file, 'w') as f:
+	f.write(travis_content)
