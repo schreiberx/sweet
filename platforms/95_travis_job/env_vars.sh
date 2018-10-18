93b8af73febf4c5daea77cb6bfe144a5caa2a749
Martin Schreiber <SchreiberX@gmail.com>
2018-10-18 12:11:42 +0200

    Added travis job build environment

---
+++ platforms/95_travis_job/env_vars.sh
@@ -0,0 +1,36 @@
+
+#
+# Default (fallback) configuration file
+#
+
+echo_warning_hline
+echo_warning "This is the fallback environment using GNU compiler environment"
+echo_warning "In the case that you just want to test SWEET, that's fine."
+echo_warning "If you want to actively develop in SWEET, think about setting"
+echo_warning "up your own platform."
+echo_warning_hline
+
+#
+# Compiler environment
+#
+#export SWEET_F90=gfortran-8
+#export SWEET_CC=gcc-8
+#export SWEET_CXX=g++-8
+
+#export SWEET_MPICC=mpicc
+#export SWEET_MPICXX=mpic+_
+#export SWEET_MPIF90=mpif90
+
+#export SWEET_LINK=$SWEET_CXX
+#export SWEET_MPILINK=$SWEET_MPICXX
+
+#
+# local software compile overrides
+#
+#export F90=$SWEET_F90
+#export FC=$F90
+#export CC=$SWEET_CC
+#export CXX=$SWEET_CXX
+#export LINK=$SWEET_CXX
+#export LD=ld
+
