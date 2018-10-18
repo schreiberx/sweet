d9a1749d299c7e938f9b3016740eab5d5bc87a33
Martin Schreiber <SchreiberX@gmail.com>
2018-10-18 23:06:30 +0200

    updated jobs

--- travis_setup_tests.py
+++ travis_setup_tests.py
@@ -40,7 +40,11 @@ jobs:
   include:
 
 """)
-	jobs_list = [
+
+	jobs_list = []
+
+	if False:
+		jobs_list += [
 """
     # Test with G++-5
     - os: linux
@@ -53,7 +57,10 @@ jobs:
             - gfortran-5
       env:
         - MATRIX_EVAL="export CC=gcc-5 && export CXX=g++-5 && export FC=gfortran-5 && export F90=gfortran-5"
-""",
+"""]
+
+	if False:
+		jobs_list += [
 """
     # Test with G++-6
     - os: linux
@@ -66,7 +73,10 @@ jobs:
             - gfortran-6
       env:
         - MATRIX_EVAL="export CC=gcc-6 && export CXX=g++-6 && export FC=gfortran-6 && export F90=gfortran-6"
-""",
+"""]
+
+	if False:
+		jobs_list += [
 """
     # Test with G++-7
     - os: linux
@@ -79,7 +89,10 @@ jobs:
             - gfortran-7
       env:
         - MATRIX_EVAL="export CC=gcc-7 && export CXX=g++-7 && export FC=gfortran-7 && export F90=gfortran-7"
-""",
+"""]
+
+	if True:
+		jobs_list += [
 """
     # Test with G++-8
     - os: linux
@@ -92,27 +105,20 @@ jobs:
             - gfortran-8
       env:
         - MATRIX_EVAL="export CC=gcc-8 && export CXX=g++-8 && export FC=gfortran-8 && export F90=gfortran-8"
-""",
-]
+"""]
 
+	c = 0
 	for (j, test) in product(jobs_list, tests):
+		j = j.replace('MATRIX_EVAL="', 'MATRIX_EVAL="TESTSCRIPT='+test+' && ')
 		f.write(j)
-		f.write("      script: ./"+test)
+#		f.write("      script: "+test)
+		f.write("      script: $TESTSCRIPT")
 		f.write("\n")
 		f.write("\n")
+		c += 1
 
 
 	f.write("""
-#
-# 3rd party libraries which should be downloaded from the ubuntu repository
-#
-addons:
-  apt:
-    sources:
-      - ubuntu-toolchain-r-test
-    packages:
-      - cmake
-
 
 #
 # Install dependencies
@@ -162,3 +168,5 @@ cache:
     local_software/local
 
 """)
+
+print("Job combinations: "+str(c))
