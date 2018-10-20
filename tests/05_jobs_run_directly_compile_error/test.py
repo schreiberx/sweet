c84a99728789c1cc2cb165462abfaf3c37fefa09
Martin Schreiber <SchreiberX@gmail.com>
2018-10-20 21:26:31 +0200

    Added job run tests for Travis

---
+++ tests/05_jobs_run_directly_compile_error/test.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+import sys
+
+from SWEET import *
+from itertools import product
+from mule.exec_program import *
+
+ie = InfoError("test")
+
+exec_program('mule.benchmark.cleanup_all', catch_output=False)
+
+"""
+Generate a job which fails.
+This assures that we are able to catch failing jobs in case of malfunctioning scripts
+"""
+
+jg = SWEETJobGeneration()
+jg.compile.program="I_DONT_EXIST"
+jg.gen_jobscript_directory()
+
+exitcode = exec_program('mule.benchmark.jobs_run_directly', catch_output=False)
+if exitcode == 0:
+	ie.error("The job should return with exit code != 0")
+	ie.error("Benchmark's return value != 0")
+	sys.exit(1)
+
+ie.success_hline()
+ie.success("The benchmark job script was intended to fail!")
+ie.success("Benchmarks successfully finished")
+ie.success_hline()
+
+exec_program('mule.benchmark.cleanup_all', catch_output=False)
+sys.exit(0)
