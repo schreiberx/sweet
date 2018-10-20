a29fffb66b7fea111dba40d77524aba4fc0eba21
Martin Schreiber <SchreiberX@gmail.com>
2018-10-21 01:03:05 +0200

    executable bit

--- tests/50_test_sphere_swe_sphere_comparison_with_reference_implementation/benchmark_gen_swe_reference_solution.py
+++ tests/50_test_sphere_swe_sphere_comparison_with_reference_implementation/benchmark_gen_swe_reference_solution.py
@@ -11,6 +11,7 @@
 import numpy as np
 import shtns
 import sys
+import stat
 import os
 
 #debug = 10
@@ -299,6 +300,7 @@ if __name__ == "__main__":
 	# Create dummy run.sh script in this folder for automized job processing
 	with open("job_benchref_solution/run.sh", "w") as rfile:
 		rfile.write("#!/bin/bash\necho\"Dummy\"")
+		os.fchmod(rfile.fileno(), stat.S_IRWXU | stat.S_IRWXG | stat.S_IROTH | stat.S_IXOTH)
 
 	def savefile(data, name, t):
 		d = x.spectogrd(data)
