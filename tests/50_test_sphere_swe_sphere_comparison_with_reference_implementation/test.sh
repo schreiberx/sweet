e86f8b6b780f3d5faedaf6f391fc558be6c8cb19
Martin Schreiber <SchreiberX@gmail.com>
2018-10-20 23:45:49 +0200

    Added SWE on sphere reference solution to the Travis scripts

--- tests/50_test_sphere_swe_sphere_comparison_with_reference_implementation/test.sh
+++ tests/50_test_sphere_swe_sphere_comparison_with_reference_implementation/test.sh
@@ -6,19 +6,17 @@ echo "Running comparisons of SWEET SWE on the sphere implementation with Python 
 echo "***********************************************"
 
 
-cd "./run_test_swe_sphere_reference_implementation" || exit
-
-./cleanup_all || exit 1
+mule.benchmark.cleanup_all || exit 1
 
 ./benchmark_create_job_scripts || exit 1
 
 ./benchmark_gen_swe_reference_solution.py || exit 1
 
-./platform_jobs_run_directly || exit 1
+mule.benchmark.jobs_run_directly || exit 1
 
 ./postprocessing || exit 1
 
-./cleanup_all || exit 1
+mule.benchmark.cleanup_all || exit 1
 
 echo "***********************************************"
 echo "***************** FIN *************************"
