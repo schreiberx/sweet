e773f4802ef4f6739d0ef18683eeae83d1996594
Martin Schreiber <SchreiberX@gmail.com>
2018-10-22 15:14:10 +0200

    added yet another test

---
+++ tests/50_test_sphere_sph_operators/test.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+import sys
+import os
+os.chdir(os.path.dirname(sys.argv[0]))
+
+from SWEET import *
+from itertools import product
+from mule.exec_program import *
+
+exec_program('mule.benchmark.cleanup_all', catch_output=False)
+
+jg = SWEETJobGeneration()
+jg.compile.unit_test="test_sphere_sph_operators"
+jg.compile.plane_spectral_space="disable"
+jg.compile.sphere_spectral_space="enable"
+
+params_runtime_mode_res = [64, 128, 256, 512, 1024, 2048]
+jg.runtime.verbosity = 5
+
+for jg.runtime.mode_res in params_runtime_mode_res:
+	jg.gen_jobscript_directory()
+
+exitcode = exec_program('mule.benchmark.jobs_run_directly', catch_output=False)
+if exitcode != 0:
+	sys.exit(exitcode)
+
+print("Benchmarks successfully finished")
+
+exec_program('mule.benchmark.cleanup_all', catch_output=False)
