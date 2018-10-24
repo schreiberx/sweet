967792e44a1ba90428f565ce2311ddb4e37cc28b
Martin Schreiber <SchreiberX@gmail.com>
2018-10-24 12:32:36 +0200

    cleanups and numa allocator dedicated tests

--- tests/10_compile/10_compile_test_advection/test.sh
+++ tests/10_compile/10_compile_test_advection/test.sh
@@ -2,14 +2,9 @@
 
 cd "$SWEET_ROOT"
 
-for i in 0 1 2 3; do
-	SCONS="scons --program=advection --numa-block-allocator=$i --threading=omp --mode=debug"
-	echo "$SCONS"
-	$SCONS || exit
-done
-
 echo
 echo "ADVECTION"
+
 
 SCONS="scons --program=advection --plane-spectral-space=enable --mode=debug"
 echo "$SCONS"
---
+++ tests/10_compile/10_compile_test_numa_block_allocator/test.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+cd "$SWEET_ROOT"
+
+for i in 0 1 2 3; do
+	SCONS="scons --program=advection --numa-block-allocator=$i --threading=omp --mode=debug"
+	echo "$SCONS"
+	$SCONS || exit
+
+	SCONS="scons --program=advection --numa-block-allocator=$i --threading=off --mode=debug"
+	echo "$SCONS"
+	$SCONS || exit
+
+	SCONS="scons --program=advection --numa-block-allocator=$i --threading=omp --mode=release"
+	echo "$SCONS"
+	$SCONS || exit
+done
+
