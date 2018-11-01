5a7ac1b7df8ea942103d262365f56e9ec2448d00
Martin Schreiber <schreiberx@gmail.com>
2018-11-01 13:49:02 +0100

    updated platforms with MPI specific compiler

--- platforms/90_default_gnu/env_vars.sh
+++ platforms/90_default_gnu/env_vars.sh
@@ -13,3 +13,10 @@ export FC=$F90
 export LD=ld
 export SWEET_LINK=$SWEET_CXX
 
+export SWEET_MPICC=mpicc
+export SWEET_MPICXX=mpic++
+export SWEET_MPIF90=mpif90
+
+export SWEET_MPILINK=mpif90
+# If we link with mpif90, we have to add stdc++ for C++
+export SWEET_MPILIBS=stdc++
