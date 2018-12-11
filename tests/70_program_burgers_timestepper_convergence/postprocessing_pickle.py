44a351e0d7ef52a1b9888b7c8fe2eaf996770077
Martin Schreiber <SchreiberX@gmail.com>
2018-12-11 07:14:12 +0100

    updated tests

--- tests/70_program_burgers_timestepper_convergence/postprocessing_pickle.py
+++ tests/70_program_burgers_timestepper_convergence/postprocessing_pickle.py
@@ -4,7 +4,7 @@ import sys
 import math
 import glob
 
-from sweet.postprocessing.pickle_PlaneDataPhysicalDiff import *
+from mule_local.postprocessing.pickle_PlaneDataPhysicalDiff import *
 from mule.exec_program import *
 
 pickle_PlaneDataPhysicalDiff()
