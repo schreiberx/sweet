ea97e5c930057dd2504f8ea9345ef520fd61f83e
Martin Schreiber <SchreiberX@gmail.com>
2019-01-20 19:44:54 +0100

    Converted kinetic energy spectrum computation to MULE

---
+++ benchmarks_plane/sl-rexi/unstablejet_new/postprocessing_kineticenergy_to_pickle.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+from mule_local.postprocessing.pickle_PlaneData_KineticEnergy import *
+from mule.exec_program import *
+
+# process only last file (likely the reference solution)
+pickle_PlaneData_KineticEnergy(params=["only_last_file", "ignore_missing_file"])
+
+# process all files
+#pickle_PlaneData_KineticEnergy(params=[])
