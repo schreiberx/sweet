559376bd186bc02f4dd3afc293a16405f86c77c4
Martin Schreiber <SchreiberX@gmail.com>
2019-01-21 01:04:02 +0100

    added unified REXI phi0 scripts

---
+++ benchmarks_generic/rexi_phi0_studies/postprocessing_plot_all.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+
+./postprocessing_plot.py output_trexi_all.pdf *FILEREXI_TREXI_*
+./postprocessing_plot.py output_cirexi_all.pdf *FILEREXI_CIREXI_*
+./postprocessing_plot.py output_brexi_all.pdf *FILEREXI_BREXI_*
