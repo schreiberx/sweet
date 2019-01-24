e8cba35a602d316dbb7d71c927f2e75e54d98fbf
Martin Schreiber <SchreiberX@gmail.com>
2019-01-24 23:37:39 +0100

    updated postprocessing scripts

--- benchmarks_plane/sl-rexi/unstablejet_new/postprocessing_plot_errors_all.sh
+++ benchmarks_plane/sl-rexi/unstablejet_new/postprocessing_plot_errors_all.sh
@@ -16,7 +16,7 @@ DATA+=" *tsm_l_rexi_na_sl_nd_etdrk_tso2*"
 DATA+=" *tsm_l_rexi_na_sl_nd_settls_tso2*"
 
 # ETD2RK
-DATA+=" *tsm_l_rexi_n_etdrk_tso2"
+DATA+=" *tsm_l_rexi_n_etdrk_tso2*"
 
 
 ./postprocessing_plot_errors_single.py plot_errors.pdf $DATA
