493b87bad7c48f918bbdf4cc1b9baaaa4df82001
Martin Schreiber <SchreiberX@gmail.com>
2019-01-26 17:46:37 +0100

    updated scripts #2

--- benchmarks_plane/sl-rexi/unstablejet_simtime1d_nldiv0_swefull/pp_pickle_errors.py
+++ benchmarks_plane/sl-rexi/unstablejet_simtime1d_nldiv0_swefull/pp_pickle_errors.py
@@ -1,6 +1,18 @@
 #! /usr/bin/env python3
 
+import sys
+
 from mule_local.postprocessing.pickle_PlaneDataPhysicalDiff import *
 from mule.exec_program import *
 
-pickle_PlaneDataPhysicalDiff(params=["interpolate_ref_to_cmp", "ignore_missing_file"])
+
+if len(sys.argv) > 1:
+	pickle_PlaneDataPhysicalDiff(
+				job_dirs=sys.argv[1:],
+				params=["interpolate_ref_to_cmp", "ignore_missing_file"]
+		)
+
+else:
+	pickle_PlaneDataPhysicalDiff(
+				params=["interpolate_ref_to_cmp", "ignore_missing_file"]
+		)
