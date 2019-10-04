79a7152283953781f51014e03d396f8affbcfcd7
Pedro Peixoto <pedrosp@ime.usp.br>
2019-10-04 13:13:18 -0300

    pedrosp at ime platform added to mules

---
+++ mule/platforms/50_pedrosp_ime_gnu/JobPlatformAutodetect.py
@@ -0,0 +1,22 @@
+
+import platform
+
+def autodetect():
+
+	"""
+	Returns
+	-------
+	bool
+		True if current platform matches, otherwise False
+	"""
+
+	prefix = "pedrosp-ime"
+	if platform.node()[:len(prefix)] == prefix:
+		return True
+
+	return False
+
+
+if __name__ == "__main__":
+	print("Autodetect: "+str(autodetect()))
+
