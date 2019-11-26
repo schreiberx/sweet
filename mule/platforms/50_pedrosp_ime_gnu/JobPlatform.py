313d1148a4aa44e4daf6d94b7bca06fd174743b5
Pedro Peixoto <pedrosp@ime.usp.br>
2019-11-26 10:47:29 -0300

    Revert "Merge branch 'master' of https://github.com/schreiberx/sweet"
    This reverts commit ea6f59e7bfb87b5cee21bc591ce2e38c854003eb, reversing
    changes made to 8fe4652a7c9bf9c25ec353efa052454f6300bf1d.

--- mule/platforms/50_pedrosp_ime_gnu/JobPlatform.py
+++ mule/platforms/50_pedrosp_ime_gnu/JobPlatform.py
@@ -61,7 +61,7 @@ def get_platform_id():
 		unique ID of platform
 	"""
 
-	return "pedrosp_ime_gnu"
+	return "guepardo_gnu"
 
 
 def get_platform_resources():
--- mule_local/platforms/50_pedrosp_ime_gnu/local_software_default.sh
+++ mule_local/platforms/50_pedrosp_ime_gnu/local_software_default.sh
@@ -17,10 +17,10 @@ done
 
 
 #PKGS+=("install_autoconf.sh")
-#PKGS+=("install_make.sh")
+PKGS+=("install_make.sh")
 PKGS+=("install_gcc8.2.sh")
 
-#PKGS+=("install_cmake.sh")
+PKGS+=("install_cmake.sh")
 #PKGS+=("install_automake.sh")
 
 PKGS+=("install_fftw3.sh")
