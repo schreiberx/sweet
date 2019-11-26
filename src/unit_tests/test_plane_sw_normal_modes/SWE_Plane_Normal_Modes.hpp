0b43c6717aa22ea14d725587a81f8466fb8b614f
Pedro Peixoto <pedrosp@ime.usp.br>
2019-11-26 10:22:33 -0300

    pedro bug on plane_data spectral get/set?

--- src/unit_tests/test_plane_sw_normal_modes/SWE_Plane_Normal_Modes.hpp
+++ src/unit_tests/test_plane_sw_normal_modes/SWE_Plane_Normal_Modes.hpp
@@ -153,32 +153,33 @@ public:
 		//for (int k = 0; k < 3; k++)
 		//	for (int j = 0; j < 3; j++)
 		//		U[k] += v[k][j] * UEV[j];
-
-		io_h.print_spectralData();
+		io_h.print_spectralIndex();
 		io_h.print_physicalArrayData();
-
-		io_h.p_spectral_set(ik1, ik0, io_h.p_spectral_get(ik1, ik0)+UEV[0]);
+		complex tmp_h = io_h.p_spectral_get(ik1, ik0)+UEV[0];
+		std::cout<<ik0<<" "<<ik1<<"tmp_h "<<tmp_h<< io_h.p_spectral_get(ik1, ik0) << UEV[0] << std::endl;
+		io_h.p_spectral_set(ik1, ik0, tmp_h);
 		io_u.p_spectral_set(ik1, ik0, io_u.p_spectral_get(ik1, ik0)+UEV[1]);
 		io_v.p_spectral_set(ik1, ik0, io_v.p_spectral_get(ik1, ik0)+UEV[2]);
 		
 		std::cout<<"spectral "<< ik1<<std::endl;
-		io_h.print_spectralData();
+		io_h.print_spectralIndex();
 		std::cout<<"physical"<<std::endl;
 		io_h.request_data_physical();
-		io_h.print_physicalArrayData();
+		io_h.print_physicalArrayData(3);
 
 		//Mirror wavenumbers planeDataConfig->spectral_data_size[1]
-		ik1 = (T)((int)planeDataConfig->spectral_data_size[1]-(int)ik1 - (int)1);
-		
-		io_h.p_spectral_set(ik1, ik0, io_h.p_spectral_get(ik1, ik0)+UEV[0]);
+		ik1 = (T)((int)planeDataConfig->spectral_data_size[1]-(int)ik1);
+		tmp_h = io_h.p_spectral_get(ik1, ik0)+UEV[0];
+		std::cout<<ik0<<" "<<ik1<<" tmp_h "<<tmp_h<< io_h.p_spectral_get(ik1, ik0) << UEV[0] << std::endl;
+		io_h.p_spectral_set(ik1, ik0, tmp_h);
 		io_u.p_spectral_set(ik1, ik0, io_u.p_spectral_get(ik1, ik0)+UEV[1]);
 		io_v.p_spectral_set(ik1, ik0, io_v.p_spectral_get(ik1, ik0)+UEV[2]);
 		
 		std::cout<<"spectral with mirror "<< ik1<<std::endl;
-		io_h.print_spectralData();
+		io_h.print_spectralIndex();
 		std::cout<<"physical"<<std::endl;
 		io_h.request_data_physical();
-		io_h.print_physicalArrayData();
+		io_h.print_physicalArrayData(3);
 	}
 
 
