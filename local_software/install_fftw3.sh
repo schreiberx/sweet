02d61618bb1571c37f773047cd262d38617561d6
Martin Schreiber <martin.schreiber@tum.de>
2020-03-09 19:28:47 +0100

    added missing file

---
+++ local_software/install_fftw3.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+source ./install_helpers.sh ""
+source ./env_vars.sh ""
+
+PKG_NAME="fftw"
+PKG_INSTALLED_FILE="$SWEET_LOCAL_SOFTWARE_DST_DIR/lib/libfftw3.a"
+PKG_URL_SRC="https://www.martin-schreiber.info/pub/sweet_local_software/fftw-3.3.8.tar.gz"
+
+config_setup
+
+config_package $@
+
+
+CONF_FLAGS=""
+
+if [ "`uname -s`" == "Linux" -o "`uname -s`" == "Darwin" ]; then
+	CONF_FLAGS+=" --enable-openmp "
+fi
+
+# Activate vectorization code
+
+#	sse only works with single precision
+#	CONF_FLAGS+=" --enable-sse"
+
+CONF_FLAGS+=" --enable-sse2"
+CONF_FLAGS+=" --enable-avx"
+CONF_FLAGS+=" --enable-avx2"
+CONF_FLAGS+=" --enable-avx512"
+#CONF_FLAGS+=" --enable-avx-128-fma"
+
+# Never used directly in Fortran
+#CONF_FLAGS+=" --disable-fortran"
+CONF_FLAGS+=" --enable-fortran"
+
+# Enable generation of shared library
+CONF_FLAGS+=" --enable-shared"
+
+#	neon only works with single precision
+#	CONF_FLAGS+=" --enable-neon"
+
+echo "Configuration flags: $CONF_FLAGS"
+
+config_configure $CONF_FLAGS
+
+config_make_default_install
+
+config_success
