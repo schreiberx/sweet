403808d4933a85cf669a0da8960d3f1284ae0d21
Martin Schreiber <martin.schreiber@tum.de>
2020-06-05 19:01:17 +0200

    updates

--- benchmarks_sphere/paper_jrn_sl_exp/pretty_plotting.py
+++ benchmarks_sphere/paper_jrn_sl_exp/pretty_plotting.py
@@ -27,9 +27,9 @@ latex_pretty_names = {
 	'sphere_data_diff_prog_h.res_norm_l2'	: '$L_2$ norm surface height (res.\,normalized)',
 	'sphere_data_diff_prog_h.res_norm_linf'	: '$L_{\infty}$ norm surface height',
 
-	'sphere_data_diff_prog_vort.res_norm_l1'	: '$L_1$ norm vorticity (res.\,normalized)',
-	'sphere_data_diff_prog_vort.res_norm_l2'	: '$L_2$ norm vorticity (res.\,normalized)',
-	'sphere_data_diff_prog_vort.res_norm_linf'	: '$L_{\infty}$ norm vorticity',
+	'sphere_data_diff_prog_vrt.res_norm_l1'	: '$L_1$ norm vorticity (res.\,normalized)',
+	'sphere_data_diff_prog_vrt.res_norm_l2'	: '$L_2$ norm vorticity (res.\,normalized)',
+	'sphere_data_diff_prog_vrt.res_norm_linf'	: '$L_{\infty}$ norm vorticity',
 
 	'sphere_data_diff_prog_div.res_norm_l1'	: '$L_1$ norm divergence (res.\,normalized)',
 	'sphere_data_diff_prog_div.res_norm_l2'	: '$L_2$ norm divergence (res.\,normalized)',
