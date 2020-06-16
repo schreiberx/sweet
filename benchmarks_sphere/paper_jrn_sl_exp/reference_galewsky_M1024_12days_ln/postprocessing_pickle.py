8eb2fc113b6aa01b7abe22fb34f43c6e8765324b
Martin Schreiber <schreiberx@gmail.com>
2020-06-16 19:04:59 +0200

    added plotting scripts for reference benchmark

--- benchmarks_sphere/paper_jrn_sl_exp/reference_galewsky_M1024_12days_ln/postprocessing_pickle.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-if 1:
-    from mule_local.postprocessing.pickle_SphereDataSpectralDiff import *
-    p = pickle_SphereDataSpectralDiff()
-
-else:
-    from mule_local.postprocessing.pickle_SphereDataPhysicalDiff import *
-    p = pickle_SphereDataPhysicalDiff()
