f735c761db2c7ea48f9281cc19d65a160a8e3ea0
Martin Schreiber <schreiberx@gmail.com>
2020-06-26 17:10:10 +0200

    intermediate commit for backups before cleaning up

---
+++ benchmarks_sphere/paper_jrn_sl_exp/reference_galewsky_M1024_12days_ln_dt0010sec/postprocessing_plot_solution.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import sys
+import numpy as np
+
+from mule.postprocessing.JobData import JobData
+from mule_local.postprocessing.SphereDataSpectral import SphereDataSpectral
+import mule_local.postprocessing.shtnsfiledata as shtnsfiledata
+from postprocessing_swe import postprocessing_swe
+
+debug_active = False
+
+print(" ".join(sys.argv))
+
+if len(sys.argv) < 5:
+    print("Usage: "+sys.argv[0]+" 'jobdirectory' 'phidatafile.sweet' 'vrtdatafile.sweet' 'divdatafile.sweet'")
+    raise Exception("Not enough arguments")
+
+s = postprocessing_swe()
+s.setup(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
+s.plot_kinetic_energy_spectrum()
+
+s.plot_phi_pert_spectrum()
+s.plot_vrt_spectrum()
+s.plot_div_spectrum()
+
+s.plot_physical_fields()
+
---
+++ benchmarks_sphere/paper_jrn_sl_exp/reference_galewsky_M1024_12days_ln_dt0010sec_NEW/postprocessing.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+#JOB_DIR=job_benchref_RT_u0.0_tsm_ln_erk_tso4_tsob4_dt00005.00_W-00001
+JOB_DIR=job_benchref_RT_u0.0_tsm_ln_erk_tso4_tsob4_dt00010.00_W-00001
+
+for i in $JOB_DIR/output_prog_phi_pert_*.sweet; do
+
+	FILE_PHI="$i"
+	FILE_VRT="${i/phi_pert/vrt}"
+	FILE_DIV="${i/phi_pert/div}"
+
+	./postprocessing_plot_solution.py "$JOB_DIR" "$FILE_PHI" "$FILE_VRT" "$FILE_DIV" || exit 1
+done
+
+
+for i in spectrum_kinetic_energy; do # spectrum_phi_pert spectrum_vrt spectrum_div output_prog_h_pert output_prog_vrt output_prog_div; do
+	pdftk $JOB_DIR/plot_$i*.pdf output ./output_$i.pdf || exit 1
+done
