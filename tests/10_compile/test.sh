6cebfbd31ae22bbecb938764a55d4dc701398f44
Martin SCHREIBER <schreiberx@gmail.com>
2020-12-30 21:59:37 +0100

    updated tests to avoid length logs for Travis CI

--- tests/10_compile/test.sh
+++ tests/10_compile/test.sh
@@ -3,8 +3,9 @@
 cd "$(dirname $0)"
 
 for i in $(ls -1 -d ??_*/); do
+	i=$(basename "$i")
 	echo_info_hline
 	echo_info "Runing compile tests for $i"
 	echo_info_hline
-	./$i/test.* || exit 1
+	./$i/test.* > output_"${i/\//}"".out" || exit 1
 done
