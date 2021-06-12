fabb4bf28a6c61499c2ca44b4c63de08a7d2eeae
Valentina Schueller <valentina.schueller@gmail.com>
2021-06-12 11:10:05 +0200

    Fix Timestamp in File Name
    - removed "current_step_n" from file name
    - instead, the file name now shows the correct current simulation time in
     t000... (similar to swe_sphere)
    - TODO: next step: provide an option to use hours instead of seconds?

--- src/programs/libpfasst_swe_sphere/chooks.cpp
+++ src/programs/libpfasst_swe_sphere/chooks.cpp
@@ -82,23 +82,26 @@ extern "C"
     const SphereData_Spectral& vort_Y = i_Y->get_vort();
     const SphereData_Spectral& div_Y  = i_Y->get_div();
 
+    // get the pointer to the Simulation Variables object
+    SimulationVariables* simVars = i_ctx->get_simulation_variables();
+    simVars->timecontrol.current_timestep_nr = i_current_step + 1;
+    auto current_dt = simVars->timecontrol.current_timestep_size;
+    simVars->timecontrol.current_simulation_time = (i_current_step + 1) * current_dt;
+
     // write the data to file
     std::string filename = "prog_jump_phi_current_proc_"+std::to_string(i_current_proc)
-                                +"_current_step_"+std::to_string(i_current_step)
                                 +"_current_iter_"+std::to_string(i_current_iter)
                                 +"_nnodes_"      +std::to_string(i_nnodes)
                                 +"_niters_"      +std::to_string(i_niters);
     write_file(*i_ctx, phi_Y, filename.c_str());
 
     filename = "prog_jump_vort_current_proc_"+std::to_string(i_current_proc)
-                    +"_current_step_"+std::to_string(i_current_step)
                     +"_current_iter_"+std::to_string(i_current_iter)
                     +"_nnodes_"      +std::to_string(i_nnodes)
                     +"_niters_"      +std::to_string(i_niters);
     write_file(*i_ctx, vort_Y, filename.c_str());
 
     filename = "prog_jump_div_current_proc_"+std::to_string(i_current_proc)
-                    +"_current_step_"+std::to_string(i_current_step)
                     +"_current_iter_"+std::to_string(i_current_iter)
                     +"_nnodes_"      +std::to_string(i_nnodes)
                     +"_niters_"      +std::to_string(i_niters);
@@ -122,23 +125,26 @@ void cecho_output_solution(
     const SphereData_Spectral& vort_Y = i_Y->get_vort();
     const SphereData_Spectral& div_Y  = i_Y->get_div();
 
+    // get the pointer to the Simulation Variables object
+    SimulationVariables* simVars = i_ctx->get_simulation_variables();
+    simVars->timecontrol.current_timestep_nr = i_current_step + 1;
+    auto current_dt = simVars->timecontrol.current_timestep_size;
+    simVars->timecontrol.current_simulation_time = (i_current_step + 1) * current_dt;
+
     // write the data to file
     std::string filename = "prog_phi_current_proc_"+std::to_string(i_current_proc)
-                                +"_current_step_"+std::to_string(i_current_step)
                                 +"_current_iter_"+std::to_string(i_current_iter)
                                 +"_nnodes_"      +std::to_string(i_nnodes)
                                 +"_niters_"      +std::to_string(i_niters);
     write_file(*i_ctx, phi_Y, filename.c_str());
 
     filename = "prog_vort_current_proc_"+std::to_string(i_current_proc)
-                    +"_current_step_"+std::to_string(i_current_step)
                     +"_current_iter_"+std::to_string(i_current_iter)
                     +"_nnodes_"      +std::to_string(i_nnodes)
                     +"_niters_"      +std::to_string(i_niters);
     write_file(*i_ctx, vort_Y, filename.c_str());
 
     filename = "prog_div_current_proc_"+std::to_string(i_current_proc)
-                    +"_current_step_"+std::to_string(i_current_step)
                     +"_current_iter_"+std::to_string(i_current_iter)
                     +"_nnodes_"      +std::to_string(i_nnodes)
                     +"_niters_"      +std::to_string(i_niters);
