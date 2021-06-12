6be3ceb4010e287d9067bc40143dff8b4d3f863d
Valentina Schueller <valentina.schueller@gmail.com>
2021-06-12 11:52:27 +0200

    Add Support for Output Frequency Flag -o
    -o output_freq now affects the output frequency of libpfasst_swe_sphere:
    output_freq < 0  --> no output in between, only at final time step
    output_freq = 0  --> output at every time step
    output_freq > 0  --> output if  (current_time mod output_freq) == 0
    
    Also did some reformatting to keep indentation consistent with rest of SWEET

--- src/programs/libpfasst_swe_sphere/chooks.cpp
+++ src/programs/libpfasst_swe_sphere/chooks.cpp
@@ -5,151 +5,163 @@
 
 extern "C"
 {
-  void cecho_error(SphereData_Spectral* sd,
-		   int step)
-  {
-    // not implemented
-  }
 
-  void cecho_residual(SphereDataCtx *i_ctx,
-		      double i_norm,
-		      int i_current_proc)
-  {
-    // get the residual vector
-    std::vector<std::vector<double> >& residuals = i_ctx->get_residuals();
+    void cecho_error(SphereData_Spectral* sd,
+                     int step)
+    {
+      // not implemented
+    }
 
-    // save the residual
-    residuals[i_current_proc].push_back(i_norm);
-  }
+    void cecho_residual(SphereDataCtx *i_ctx,
+                        double i_norm,
+                        int i_current_proc)
+    {
+        // get the residual vector
+        std::vector<std::vector<double> >& residuals = i_ctx->get_residuals();
 
-  void cecho_output_invariants(
-			       SphereDataCtx *i_ctx,
-			       SphereDataVars *i_Y,
-			       int i_current_proc,
-			       int i_current_step,
-			       int i_current_iter,
-			       int i_nnodes,
-			       int i_niters
-			       )
-  {
-    const SphereData_Spectral& phi_Y  = i_Y->get_phi();
-    const SphereData_Spectral& vort_Y = i_Y->get_vort();
-    const SphereData_Spectral& div_Y  = i_Y->get_div();
+        // save the residual
+        residuals[i_current_proc].push_back(i_norm);
+    }
 
-    // get the current space-time level
-    const int level = i_Y->get_level();
+    void cecho_output_invariants(SphereDataCtx *i_ctx,
+                                 SphereDataVars *i_Y,
+                                 int i_current_proc,
+                                 int i_current_step,
+                                 int i_current_iter,
+                                 int i_nnodes,
+                                 int i_niters
+                                 )
+    {
+        const SphereData_Spectral& phi_Y  = i_Y->get_phi();
+        const SphereData_Spectral& vort_Y = i_Y->get_vort();
+        const SphereData_Spectral& div_Y  = i_Y->get_div();
 
-    // get the simulation variables
-    SimulationVariables* simVars         = i_ctx->get_simulation_variables();
+        // get the current space-time level
+        const int level = i_Y->get_level();
 
-    // get the SphereDiagnostics object from context
-    SphereHelpers_Diagnostics* sphereDiagnostics = i_ctx->get_sphere_diagnostics();
+        // get the simulation variables
+        SimulationVariables* simVars         = i_ctx->get_simulation_variables();
 
-    // get the SphereOperators object from context
-    SphereOperators_SphereData* sphereOperators     = i_ctx->get_sphere_operators(level);
+        // get the SphereDiagnostics object from context
+        SphereHelpers_Diagnostics* sphereDiagnostics = i_ctx->get_sphere_diagnostics();
 
-    // compute the invariants
-    sphereDiagnostics->update_phi_vrt_div_2_mass_energy_enstrophy(
-								   *sphereOperators,
-								   phi_Y,
-								   vort_Y,
-								   div_Y,
-								   *simVars
-								   );
+        // get the SphereOperators object from context
+        SphereOperators_SphereData* sphereOperators     = i_ctx->get_sphere_operators(level);
 
-    std::cout << std::setprecision(20) 
-	      << "mass = " << simVars->diag.total_mass
-	      << " energy = " << simVars->diag.total_energy
-	      << " potential_enstrophy = " << simVars->diag.total_potential_enstrophy
-	      << std::endl;
-    
-    // save the invariants for plotting at the end
-    i_ctx->save_physical_invariants(i_current_step);
+        // compute the invariants
+        sphereDiagnostics->update_phi_vrt_div_2_mass_energy_enstrophy(
+                                       *sphereOperators,
+                                       phi_Y,
+                                       vort_Y,
+                                       div_Y,
+                                       *simVars
+                                       );
 
-  }
+        std::cout << std::setprecision(20)
+              << "mass = " << simVars->diag.total_mass
+              << " energy = " << simVars->diag.total_energy
+              << " potential_enstrophy = " << simVars->diag.total_potential_enstrophy
+              << std::endl;
 
-  void cecho_output_jump(
-			 SphereDataCtx *i_ctx,
-			 SphereDataVars *i_Y,
-			 int i_current_proc,
-			 int i_current_step,
-			 int i_current_iter,
-			 int i_nnodes,
-			 int i_niters
-			 )
-  {
-    const SphereData_Spectral& phi_Y  = i_Y->get_phi();
-    const SphereData_Spectral& vort_Y = i_Y->get_vort();
-    const SphereData_Spectral& div_Y  = i_Y->get_div();
+        // save the invariants for plotting at the end
+        i_ctx->save_physical_invariants(i_current_step);
+    }
 
-    // get the pointer to the Simulation Variables object
-    SimulationVariables* simVars = i_ctx->get_simulation_variables();
-    simVars->timecontrol.current_timestep_nr = i_current_step + 1;
-    auto current_dt = simVars->timecontrol.current_timestep_size;
-    simVars->timecontrol.current_simulation_time = (i_current_step + 1) * current_dt;
+    void cecho_output_jump(SphereDataCtx *i_ctx,
+                           SphereDataVars *i_Y,
+                           int i_current_proc,
+                           int i_current_step,
+                           int i_current_iter,
+                           int i_nnodes,
+                           int i_niters
+                           )
+    {
+        const SphereData_Spectral& phi_Y  = i_Y->get_phi();
+        const SphereData_Spectral& vort_Y = i_Y->get_vort();
+        const SphereData_Spectral& div_Y  = i_Y->get_div();
 
-    // write the data to file
-    std::string filename = "prog_jump_phi_current_proc_"+std::to_string(i_current_proc)
-                                +"_current_iter_"+std::to_string(i_current_iter)
-                                +"_nnodes_"      +std::to_string(i_nnodes)
-                                +"_niters_"      +std::to_string(i_niters);
-    write_file(*i_ctx, phi_Y, filename.c_str());
+        // get the pointer to the Simulation Variables object
+        SimulationVariables* simVars = i_ctx->get_simulation_variables();
+        simVars->timecontrol.current_timestep_nr = i_current_step + 1;
+        auto current_dt = simVars->timecontrol.current_timestep_size;
+        simVars->timecontrol.current_simulation_time = (i_current_step + 1) * current_dt;
 
-    filename = "prog_jump_vort_current_proc_"+std::to_string(i_current_proc)
-                    +"_current_iter_"+std::to_string(i_current_iter)
-                    +"_nnodes_"      +std::to_string(i_nnodes)
-                    +"_niters_"      +std::to_string(i_niters);
-    write_file(*i_ctx, vort_Y, filename.c_str());
+        // write the data to file
+        std::string filename = "prog_jump_phi_current_proc_"+std::to_string(i_current_proc)
+                                    +"_current_iter_"+std::to_string(i_current_iter)
+                                    +"_nnodes_"      +std::to_string(i_nnodes)
+                                    +"_niters_"      +std::to_string(i_niters);
+        write_file(*i_ctx, phi_Y, filename.c_str());
 
-    filename = "prog_jump_div_current_proc_"+std::to_string(i_current_proc)
-                    +"_current_iter_"+std::to_string(i_current_iter)
-                    +"_nnodes_"      +std::to_string(i_nnodes)
-                    +"_niters_"      +std::to_string(i_niters);
-    write_file(*i_ctx, div_Y, filename.c_str());
+        filename = "prog_jump_vort_current_proc_"+std::to_string(i_current_proc)
+                        +"_current_iter_"+std::to_string(i_current_iter)
+                        +"_nnodes_"      +std::to_string(i_nnodes)
+                        +"_niters_"      +std::to_string(i_niters);
+        write_file(*i_ctx, vort_Y, filename.c_str());
 
-  }
+        filename = "prog_jump_div_current_proc_"+std::to_string(i_current_proc)
+                        +"_current_iter_"+std::to_string(i_current_iter)
+                        +"_nnodes_"      +std::to_string(i_nnodes)
+                        +"_niters_"      +std::to_string(i_niters);
+        write_file(*i_ctx, div_Y, filename.c_str());
+    }
 
 
 
-void cecho_output_solution(
-			     SphereDataCtx *i_ctx,
-			     SphereDataVars *i_Y,
-			     int i_current_proc,
-			     int i_current_step,
-			     int i_current_iter,
-			     int i_nnodes,
-			     int i_niters
-			     )
-  {
-    const SphereData_Spectral& phi_Y  = i_Y->get_phi();
-    const SphereData_Spectral& vort_Y = i_Y->get_vort();
-    const SphereData_Spectral& div_Y  = i_Y->get_div();
+    void cecho_output_solution(SphereDataCtx *i_ctx,
+                               SphereDataVars *i_Y,
+                               int i_current_proc,
+                               int i_current_step,
+                               int i_current_iter,
+                               int i_nnodes,
+                               int i_niters
+                               )
+    {
+        // get the pointer to the Simulation Variables object
+        SimulationVariables* simVars = i_ctx->get_simulation_variables();
 
-    // get the pointer to the Simulation Variables object
-    SimulationVariables* simVars = i_ctx->get_simulation_variables();
-    simVars->timecontrol.current_timestep_nr = i_current_step + 1;
-    auto current_dt = simVars->timecontrol.current_timestep_size;
-    simVars->timecontrol.current_simulation_time = (i_current_step + 1) * current_dt;
+        // update timecontrol information
+        simVars->timecontrol.current_timestep_nr = i_current_step + 1;
+        auto current_dt = simVars->timecontrol.current_timestep_size;
+        simVars->timecontrol.current_simulation_time = (i_current_step + 1) * current_dt;
 
-    // write the data to file
-    std::string filename = "prog_phi_current_proc_"+std::to_string(i_current_proc)
-                                +"_current_iter_"+std::to_string(i_current_iter)
-                                +"_nnodes_"      +std::to_string(i_nnodes)
-                                +"_niters_"      +std::to_string(i_niters);
-    write_file(*i_ctx, phi_Y, filename.c_str());
+        // check if output is necessary
+        const auto output_dt = simVars->iodata.output_each_sim_seconds;
+        if (output_dt < 0) {
+            // no output required
+            return;
+        }
+        if (output_dt != 0) {
+            // == 0 means output at every step
+            if (std::fmod(simVars->timecontrol.current_simulation_time, output_dt) != 0) {
+                // we haven't reached the next output time yet -> no output required
+                return;
+            }
+        }
 
-    filename = "prog_vort_current_proc_"+std::to_string(i_current_proc)
-                    +"_current_iter_"+std::to_string(i_current_iter)
-                    +"_nnodes_"      +std::to_string(i_nnodes)
-                    +"_niters_"      +std::to_string(i_niters);
-    write_file(*i_ctx, vort_Y, filename.c_str());
+        const SphereData_Spectral& phi_Y  = i_Y->get_phi();
+        const SphereData_Spectral& vort_Y = i_Y->get_vort();
+        const SphereData_Spectral& div_Y  = i_Y->get_div();
 
-    filename = "prog_div_current_proc_"+std::to_string(i_current_proc)
-                    +"_current_iter_"+std::to_string(i_current_iter)
-                    +"_nnodes_"      +std::to_string(i_nnodes)
-                    +"_niters_"      +std::to_string(i_niters);
-    write_file(*i_ctx, div_Y, filename.c_str());
+        // write the data to file
+        std::string filename = "prog_phi_current_proc_"+std::to_string(i_current_proc)
+                                  +"_current_iter_"+std::to_string(i_current_iter)
+                                  +"_nnodes_"      +std::to_string(i_nnodes)
+                                  +"_niters_"      +std::to_string(i_niters);
+        write_file(*i_ctx, phi_Y, filename.c_str());
 
-  }
+        filename = "prog_vort_current_proc_"+std::to_string(i_current_proc)
+                      +"_current_iter_"+std::to_string(i_current_iter)
+                      +"_nnodes_"      +std::to_string(i_nnodes)
+                      +"_niters_"      +std::to_string(i_niters);
+        write_file(*i_ctx, vort_Y, filename.c_str());
+
+        filename = "prog_div_current_proc_"+std::to_string(i_current_proc)
+                      +"_current_iter_"+std::to_string(i_current_iter)
+                      +"_nnodes_"      +std::to_string(i_nnodes)
+                      +"_niters_"      +std::to_string(i_niters);
+        write_file(*i_ctx, div_Y, filename.c_str());
+
+    }
 
 }
