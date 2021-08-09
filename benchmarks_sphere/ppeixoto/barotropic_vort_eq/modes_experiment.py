91b7f07447fe251da0387da259d06f5d4fb88052
Pedro Peixoto <ppeixoto@usp.br>
2021-08-09 15:02:06 -0300

    update on evolution of modes by shells - postprocessing

--- benchmarks_sphere/ppeixoto/barotropic_vort_eq/modes_experiment.py
+++ benchmarks_sphere/ppeixoto/barotropic_vort_eq/modes_experiment.py
@@ -4,6 +4,17 @@
 # ----------------------------------------
 import numpy as np
 import pickle
+from numpy.lib.function_base import append
+import pandas as pd
+import re
+
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from matplotlib.lines import Line2D
+
+from mule.postprocessing.JobData import *
 
 class modes:
     def __init__(self, n_ini, n_end, m_ini, alpha_min, alpha_max, alpha_samples):
@@ -61,3 +72,198 @@ def load_file(filename):
     obj = pickle.load(f)
     f.close()        
     return obj
+
+class evol:
+    def __init__(self, basedir=".", eps=0.0001):
+
+        self.basedir = basedir
+        self.energy_file    = basedir+"/output_spec_energy_t00000000000.00000000.txt"    
+        self.enstrophy_file = basedir+"/output_spec_enstrophy_t00000000000.00000000.txt"
+        self.scalesmin = {}
+        self.scalesmax = {}
+        timerescale=1.0
+
+        #Remove modes with null values
+
+        self.df_energy=pd.read_csv(self.energy_file, sep='\t', skipinitialspace=True, skiprows=1, header=3, engine="python")
+        #print(self.df_energy)
+        self.df_ens=pd.read_csv(self.enstrophy_file, sep='\t', skipinitialspace=True, skiprows=1, header=3, engine="python")
+        #print(self.df_ens)
+
+        self.df_energy['timestamp'] = self.df_energy['timestamp'] * timerescale
+        maxenergy = self.df_energy['TotalSum'].max()
+        eps_en=maxenergy*eps
+        self.scalesmin[0]=eps_en
+        self.scalesmax[0]=maxenergy
+        self.df_energy_clean = self.df_energy.loc[:, (self.df_energy > eps_en).any(axis=0)]
+        self.df_energy_clean = self.df_energy_clean.set_index("timestamp")
+        #print(self.df_energy)
+        #df_energy.set_index('timestamp',drop=True,inplace=True)
+        print(self.df_energy_clean)
+
+        self.df_ens['timestamp'] = self.df_ens['timestamp'] * timerescale
+        maxens = self.df_ens['TotalSum'].max()
+        eps_ens=maxens*eps
+        self.scalesmin[1]=eps_ens
+        self.scalesmax[1]=maxens
+        self.df_ens_clean = self.df_ens.loc[:, (self.df_ens > eps_ens).any(axis=0)]
+        self.df_ens_clean = self.df_ens_clean.set_index("timestamp")
+        #print(self.df_ens)
+        #df_energy.set_index('timestamp',drop=True,inplace=True)
+        print(self.df_ens_clean)
+
+
+    def set_out_shells(self, nmin, nmax):
+        self.df_energy_agg = self.set_out_shells_df(self.df_energy_clean, nmin, nmax)
+        self.df_ens_agg = self.set_out_shells_df(self.df_ens_clean, nmin, nmax)
+
+    def set_out_shells_df(self, df, nmin, nmax):
+
+        tol = 10e-16
+
+        #get modes of initial energy 
+        init_modes = []
+        non_init_modes = []
+        out_modes = []
+
+        ninit_min = 9999
+        ninit_max = 0
+        for col in df.columns:
+            
+            if col == "timestamp" or col == "TotalSum":
+                continue
+            a = [int(s) for s in re.findall(r'\b\d+\b', col)]
+            n = a[0]
+            m = a[1]
+            #print(df[col].iloc[0])
+            if df[col].iloc[0] > tol:
+                init_modes.append(col)
+                #print(col, n, m, "init")
+                if n > ninit_max:
+                    ninit_max = n
+                if n < ninit_min:
+                    ninit_min = n
+            else:
+                non_init_modes.append(col)
+                #print(col, n, m, "non-init")
+
+            if n >= nmin and n <= nmax:
+                out_modes.append(col)
+                #print(col, n, m, "out")
+            
+        df_init = df[init_modes].sum(axis=1).rename("init_n"+str(ninit_min)+"-"+str(ninit_max))
+        df_noninit = df[non_init_modes].sum(axis=1).rename("non_init")
+        df_out = df[out_modes].sum(axis=1).rename("out_n"+str(nmin)+"-"+str(nmax))
+        
+        df_new = pd.concat([df_init, df_noninit, df_out], axis=1)
+
+        return df_new
+
+    def plot(self, title="", output_filename="out.pdf"):
+
+        
+        fontsize=18
+        figsize=(10, 10)
+
+        fig, axs = plt.subplots(2, figsize=(10,10))#, sharex=True)
+        plt.rc('text', usetex=False)
+        title="SPH Mode Nonlinear Interaction\n"+title
+        
+        fig.suptitle(title)
+
+        
+        for i, ax in enumerate(axs):
+            ax.set_xscale("linear")
+            ax.set_yscale("log", nonpositive='clip')
+            ylim=[self.scalesmin[i], self.scalesmax[i]]
+            ax.set_ylim(ylim)
+
+        #for ax in axs.flat:
+        #    ax.label_outer()
+
+        colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+
+        markers = []
+        for m in Line2D.markers:
+            try:
+                if len(m) == 1 and m != ' ' and m != '':
+                    markers.append(m)
+            except TypeError:
+                pass
+
+        linestyles = ['-', '--', ':', '-.']
+
+        
+        ncol = 2	
+        
+        self.df_energy_clean.plot( ax=axs[0])
+        axs[0].set(ylabel='Energy', xlabel="Time (hours)")
+        axs[0].legend(loc='center left', bbox_to_anchor= (1.01, 0.5), ncol=ncol)
+        
+        ncol=2
+    
+        self.df_ens_clean.plot( ax=axs[1])
+        axs[1].set(ylabel='Enstrophy', xlabel="Time (hours)")
+        axs[1].legend(loc='center left', bbox_to_anchor= (1.01, 0.5), ncol=ncol)
+
+        fig.subplots_adjust(right=0.7)
+        
+        print(self.basedir+"/"+output_filename)
+        plt.show()
+        plt.savefig(self.basedir+"/"+output_filename, transparent=True) #, bbox_inches='tight') #, pad_inches=0.02)
+
+        plt.close()
+
+    def plot_shells(self, title="", output_filename="out.pdf"):
+
+        fontsize=18
+        figsize=(10, 10)
+
+        fig, axs = plt.subplots(2, figsize=(10,10))#, sharex=True)
+        plt.rc('text', usetex=False)
+        title="SPH Mode Nonlinear Interaction\n"+title
+        
+        fig.suptitle(title)
+
+        for i, ax in enumerate(axs):
+            ax.set_xscale("linear")
+            ax.set_yscale("log", nonpositive='clip')
+            ylim=[self.scalesmin[i], self.scalesmax[i]]
+            ax.set_ylim(ylim)
+
+        #for ax in axs.flat:
+        #    ax.label_outer()
+
+        colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+
+        markers = []
+        for m in Line2D.markers:
+            try:
+                if len(m) == 1 and m != ' ' and m != '':
+                    markers.append(m)
+            except TypeError:
+                pass
+
+        linestyles = ['-', '--', ':', '-.']
+
+        
+        ncol = 2	
+        
+        self.df_energy_agg.plot( ax=axs[0])
+        axs[0].set(ylabel='Energy', xlabel="Time (hours)")
+        axs[0].legend(loc='center left', bbox_to_anchor= (1.01, 0.5), ncol=ncol)
+        
+        ncol=2
+    
+        self.df_ens_agg.plot( ax=axs[1])
+        axs[1].set(ylabel='Enstrophy', xlabel="Time (hours)")
+        axs[1].legend(loc='center left', bbox_to_anchor= (1.01, 0.5), ncol=ncol)
+
+        fig.subplots_adjust(right=0.7)
+        
+        print(self.basedir+"/"+output_filename)
+        plt.show()
+        plt.savefig(self.basedir+"/"+output_filename, transparent=True) #, bbox_inches='tight') #, pad_inches=0.02)
+
+        plt.close()
+
--- benchmarks_sphere/ppeixoto/barotropic_vort_eq/pp_mode_transfer_efficiency.py
+++ benchmarks_sphere/ppeixoto/barotropic_vort_eq/pp_mode_transfer_efficiency.py
@@ -64,8 +64,8 @@ for key, job in jobs.items():
 		
 
 for i in range(len(alphas)):
-	print(alphas[i], job_dirs[i])
-
+	print()
+	print("Post-processing (alpha, dir):",	alphas[i], job_dirs[i])
 
 	#List all parameters of job
 	jd_flat = jobs_flat[i]
@@ -76,126 +76,36 @@ for i in range(len(alphas)):
 	output=jd_raw['output']
 	runtime=jd_raw['jobgeneration']
 	runtime=runtime['runtime']
+	code=output['benchmark_barotropic_vort_modes.code']
+	maxmodes = int(output["benchmark_barotropic_vort_modes.maxmodes"])
 
-print(runtime)
+	nmodes=[]
+	mmodes=[]
+	ampls=[]
 
+	print("Initial conditions")
+	print("Mode, n, m, amplitude")
+	for i in range(maxmodes):
+		mode="benchmark_barotropic_vort_modes."+str(i)+"."
+		imode=i
+		nmode=int(output[mode+"nmode"])
+		mmode=int(output[mode+"mmode"])
+		ampl=float(output[mode+"ampl"])
+		print(i, nmode, mmode, ampl)
+		nmodes.append(nmode)
+		mmodes.append(mmode)
+		ampls.append(ampl)
 
-#modes_experiment=mexp.load_file()
-#print(modes_experiment.nmodes)
-exit(1)
-code=output['benchmark_barotropic_vort_modes.code']
-maxmodes = int(output["benchmark_barotropic_vort_modes.maxmodes"])
-nmodes=[]
-mmodes=[]
-ampls=[]
+	evol = mexp.evol(jd_flat['runtime.p_job_dirpath'])
 
-print("Initial conditions")
-print("Mode, n, m, amplitude")
-for i in range(maxmodes):
-	mode="benchmark_barotropic_vort_modes."+str(i)+"."
-	imode=i
-	nmode=int(output[mode+"nmode"])
-	mmode=int(output[mode+"mmode"])
-	ampl=float(output[mode+"ampl"])
-	print(i, nmode, mmode, ampl)
-	nmodes.append(nmode)
-	mmodes.append(mmode)
-	ampls.append(ampl)
+	nout_shell_min = 3
+	nout_shell_max = 6
+	evol.set_out_shells(nout_shell_min,nout_shell_max)
+	
+	evol.plot(code, "mode_evol.pdf")
 
+	filename_shell = "shell_evol_n"+str(nout_shell_min)+"-"+str(nout_shell_max)+".pdf"
+	evol.plot_shells(code, filename_shell)
 
-
-#Read output_nm files
-energy_file    = jd_flat['runtime.p_job_dirpath']+"/output_spec_energy_t00000000000.00000000.txt"
-enstrophy_file = jd_flat['runtime.p_job_dirpath']+"/output_spec_enstrophy_t00000000000.00000000.txt"
-
-
-scalesmin = {}
-scalesmax = {}
-timerescale=1.0
-#Remove modes with null values
-exclude = ['timestamp']
-
-df_energy=pd.read_csv(energy_file, sep='\t', skipinitialspace=True, skiprows=1, header=3, engine="python")
-print(df_energy)
-df_ens=pd.read_csv(enstrophy_file, sep='\t', skipinitialspace=True, skiprows=1, header=3, engine="python")
-print(df_ens)
-
-time = df_energy['timestamp']
-tenergy = df_energy['TotalSum']
-maxenergy = df_energy['TotalSum'].max()
-eps_en=maxenergy/100
-scalesmin[0]=eps_en
-scalesmax[0]=maxenergy
-df_energy_clean = df_energy.loc[:, (df_energy > eps_en).any(axis=0)]
-print(df_energy)
-#df_energy.set_index('timestamp',drop=True,inplace=True)
-print(df_energy_clean)
-
-tens = df_ens['TotalSum']
-maxens = df_ens['TotalSum'].max()
-eps_ens=maxens/100
-scalesmin[1]=eps_ens
-scalesmax[1]=maxens
-df_ens_clean = df_ens.loc[:, (df_ens > eps_ens).any(axis=0)]
-print(df_ens)
-#df_energy.set_index('timestamp',drop=True,inplace=True)
-print(df_ens_clean)
-
-##########################################################
-# Plotting starts here
-##########################################################
-
-print("*"*80)
-print("*"*80)
-print("*"*80)
-
-
-fontsize=18
-figsize=(10, 10)
-
-fig, axs = plt.subplots(2, figsize=(10,10), sharex=True)
-#plt.rc('text', usetex=True)
-title="SPH Mode Nonlinear Interaction\n"+code
-fig.suptitle(title)
-
-
-for i, ax in enumerate(axs):
-	ax.set_xscale("linear")
-	ax.set_yscale("log", nonpositive='clip')
-	ylim=[scalesmin[i], scalesmax[i]]
-	ax.set_ylim(ylim)
-
-for ax in axs.flat:
-    ax.label_outer()
-
-colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
-
-markers = []
-for m in Line2D.markers:
-    try:
-        if len(m) == 1 and m != ' ' and m != '':
-            markers.append(m)
-    except TypeError:
-        pass
-
-linestyles = ['-', '--', ':', '-.']
-
-
-ncol = 2	
-axs[0].set(ylabel='Energy', xlabel="time")
-df_energy_clean.plot(x='timestamp', ax=axs[0])
-axs[0].legend(loc='center left', bbox_to_anchor= (1.01, 0.5), ncol=ncol)
-
-
-ncol=2
-axs[1].set(ylabel='Enstrophy', xlabel="time")
-df_ens_clean.plot(x='timestamp', ax=axs[1])
-axs[1].legend(loc='center left', bbox_to_anchor= (1.01, 0.5), ncol=ncol)
-
-fig.subplots_adjust(right=0.7)
-
-plt.savefig(output_filename, transparent=True) #, bbox_inches='tight') #, pad_inches=0.02)
-
-plt.close()
-
+	#exit(1)
 
