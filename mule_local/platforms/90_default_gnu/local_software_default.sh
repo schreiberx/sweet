55c3995abc070f876374d76905730c909d455093
Martin Schreiber <schreiberx@gmail.com>
2022-07-08 15:29:35 +0200

    Updates for plots on the sphere

--- mule_local/platforms/90_default_gnu/local_software_default.sh
+++ mule_local/platforms/90_default_gnu/local_software_default.sh
@@ -29,6 +29,7 @@ PKGS+=("install_eigen3.sh")
 PKGS+=("install_cmake.sh")
 PKGS+=("install_lapack.sh")
 PKGS+=("install_numactl.sh")
+PKGS+=("install_libpfasst.sh")
 #PKGS+=("install_openssl.sh")
 
 PKGS+=("install_shtns.sh")
