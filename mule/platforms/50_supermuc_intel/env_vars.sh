2ea3f5092ef455aee2422e3ddb8b0d531c9162d6
Martin Schreiber <schreiberx@gmail.com>
2022-07-14 18:32:00 +0200

    added supermuc platform

---
+++ mule/platforms/50_supermuc_intel/env_vars.sh
@@ -0,0 +1,38 @@
+#
+# Default configuration file
+#
+
+
+#
+# Load this Python module to provide Python.h header file
+# The regular python3 include directory is messed up
+#
+
+#module unload python
+#module load python/3.6_intel
+
+
+export MULE_JOB_SCHEDULER_NUM_JOB_LIMITATION=30
+export MULE_JOB_SCHEDULER_SLEEP_SECS=30
+
+
+#
+# Compiler environment
+#
+export F90=ifort
+export CC=icc
+export CXX=icpc
+export FC=$F90
+export LD=ld
+export MULE_LINK=$MULE_CXX
+
+export MULE_MPICC=mpicc
+export MULE_MPICXX=mpicxx
+export MULE_MPIF90=mpif90
+
+
+export MULE_MPILINK=mpicxx
+# If we link with mpif90, we have to add stdc++ for C++
+#export MULE_MPILIBS=stdc++
+export MULE_MPILIBS=gfortran
+
