b82baa0604cf608d17c0b7af7b217067b8f84351
Martin Schreiber <schreiberx@gmail.com>
2023-01-03 22:12:32 +0100

    minor updates for supermuc, added threaded scalability benchmark

--- local_software/install_scons3.sh
+++ local_software/install_scons3.sh
@@ -4,6 +4,17 @@ source ./install_helpers.sh ""
 
 PKG_NAME="SCons"
 
+
+if [ "${MULE_PLATFORM_ID:0:9}" == "supermuc_" ]; then
+	echo_error ""
+	echo_error "Can't execute:"
+	echo_error "	pip3 install scons"
+	echo_error ""
+	echo_error "You need to install 'scons' manually since there's no direct internet connection!"
+	echo_error ""
+	exit 1
+fi
+
 if [ "`uname`" == "Darwin" ]; then
 	brew install scons
 else
