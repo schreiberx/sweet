9e2d7a710fd3c717524d1faeb0b0cb9b1f649829
Martin Schreiber <schreiberx@gmail.com>
2023-01-04 11:16:19 +0100

    default local software for CI

--- mule_local/platforms/10_gitlab_ci/local_software_default.sh
+++ mule_local/platforms/10_gitlab_ci/local_software_default.sh
@@ -1,37 +1,19 @@
 
-# Install binutils since they are outdated on Travis
+#PKGS+=("install_numactl.sh")
+#PKGS+=("install_miniconda.sh")
+#PKGS+=("install_fftw3.sh")
 
-# prereq: texinfo package
-#PKGS+=("install_make.sh")
-# prereq: make
-#PKGS+=("install_binutils.sh")
-
-#PKGS+=("install_openssl.sh")
-#PKGS+=("install_cacerts.sh")
-# prereq: openssl, cacerts
-
-# REMOVE ME AGAIN
-PKGS+=("install_numactl.sh")
-return
-
-PKGS+=("install_miniconda.sh")
-
-PKGS+=("install_fftw3.sh")
-
-# prereq: binutils, fftw, python
 PKGS+=("install_shtns.sh")
 PKGS+=("install_shtns_python.sh")
 
-
-PKGS+=("install_lapack.sh")
-
-PKGS+=("install_eigen3.sh")
-
+#PKGS+=("install_lapack.sh")
+#PKGS+=("install_eigen3.sh")
 #PKGS+=("install_likwid.sh")
-PKGS+=("install_numactl.sh")
 
 PKGS+=("install_scons3.sh")
 
 #PKGS+=("install_mpich.sh")
 #PKGS+=("install_libpfasst.sh")
 PKGS+=("install_libpfasst_debug.sh")
+PKGS+=("install_xbraid.sh")
+
