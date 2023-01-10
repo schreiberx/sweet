8f7928c86a2e6cdd278fced4f424ab985f363c69
Martin Schreiber <schreiberx@gmail.com>
2023-01-11 00:01:22 +0100

    merged test scripts together to work more efficient in CI

---
+++ tests/50_test_plane_GATHERED/test.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+cd "$(dirname $0)"
+
+for i in $(ls -1 -d ??_*/ | sort); do
+	i=$(basename "$i")
+	echo_info_hline
+	echo_info "Running compile tests for $i"
+	echo_info_hline
+	OUTFILE=output_"${i/\//}"".out"
+	./$i/test.* > $OUTFILE 2>&1 || { cat "$OUTFILE"; exit 1; }
+done
