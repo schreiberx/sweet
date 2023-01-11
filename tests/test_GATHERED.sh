4d92ef3ae1ec9ad209f34126fbb9714e0a723521
Martin Schreiber <schreiberx@gmail.com>
2023-01-11 09:48:26 +0100

    fixed test script

---
+++ tests/test_GATHERED.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+cd "$(dirname $0)"
+
+for i in $(ls -1 -d ??_*/ | sort); do
+	i=$(basename "$i")
+	if [[ $i == *_no_test ]]; then
+		continue
+	fi
+	echo_info_hline
+	echo_info "Running compile tests for $i"
+	echo_info_hline
+	OUTFILE=output_"${i/\//}"".out"
+	./$i/test.* > $OUTFILE 2>&1 || { cat "$OUTFILE"; exit 1; }
+done
