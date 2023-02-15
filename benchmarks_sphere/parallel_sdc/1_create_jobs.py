5c860fc0ccc4f442baeb092f65539ac68979c789
Thibaut Lunet <thibaut.lunet@tuhh.de>
2023-02-15 16:11:41 +0100

    TL: reformated parallel_sdc benchmark

--- benchmarks_sphere/parallel_sdc/1_create_jobs.py
+++ benchmarks_sphere/parallel_sdc/1_create_jobs.py
@@ -9,54 +9,17 @@ from mule.sdc import getSDCSetup
 p = JobGeneration()
 verbose = True
 
-# -------------------------------------------------------------------------------------------------
-# SDC specifics parameters
-# -------------------------------------------------------------------------------------------------
-listParamsSDC = [
-    (3, 'RADAU-RIGHT', 'BE', 'FE'),  # Default IMEX SDC (Fast Wave Slow Wave)
-    (3, 'RADAU-RIGHT', 'BEpar', 'PIC'),  # Basic parallel SDC
-    (3, 'RADAU-RIGHT', 'OPT-QmQd-0', 'PIC', 'BEpar')  # Targeted optimized parallel SDC
-]
-#paramsSDC = getSDCSetup(*listParamsSDC[1]) # => returns associated SWEETFileDict
-
-"""
-param 1: Number of support points
-param 2: Typeof quadrature nodes
-param 3: Linear term: Implicit QDelta matrix
-param 4: Nonlinear term: Explicit QDelta matrix
-param 5: QDelta matrix for initial sweep
-
-
-QDelta options:
-'PIC': Forward Euler
-
-Standard IMEX SDC
- * param 3: 'BE': Backward Euler
- * param 4: 'FE': Forward Euler
-
-Parallel SDC
- * param 3: 'BEpar': Backward Euler / parallel
- * param 4: 'PIC': Picard
-
-"""
-
-paramsSDC = getSDCSetup(3, 'RADAU-RIGHT', 'PIC', 'PIC', 'BEpar') # => returns associated SWEETFileDict
-
-# Additional parameters
-# - nIter (int) : number of sweep (can be 0)
-paramsSDC['nIter'] = 4
-
-# - diagonal (bool) : to use diagonal implementation
-paramsSDC['diagonal'] = 1
-
-# - qDeltaInit (bool) : to use qDeltaI (and qDeltaE) for initial sweep
-paramsSDC['qDeltaInit'] = 0
-
-# - useEndUpdate (bool) : to use collocation formula for end-update solution
-paramsSDC['useEndUpdate'] = 0
-
-p.runtime.paramsSDC = paramsSDC
-# -------------------------------------------------------------------------------------------------
+p.runtime.paramsSDC = getSDCSetup(
+    nNodes=3,
+    nIter=3,
+    nodeType='RADAU-RIGHT', 
+    qDeltaImplicit='OPT-QmQd-0', 
+    qDeltaExplicit='PIC', 
+    diagQDeltaInit='BEpar',
+    diagonal=1,
+    initSweepType="QDELTA",
+    useEndUpdate=0
+)
 
 p.compile.mode = 'release'
 p.compile.gui = 'enable'
