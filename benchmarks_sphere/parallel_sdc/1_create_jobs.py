072e125707013c922919474b01169e20b9a2d503
Thibaut Lunet <thibaut.lunet@tuhh.de>
2023-02-15 16:31:11 +0100

    TL: same settings as stable1200

--- benchmarks_sphere/parallel_sdc/1_create_jobs.py
+++ benchmarks_sphere/parallel_sdc/1_create_jobs.py
@@ -46,7 +46,7 @@ p.runtime.output_timestep_size = 60*60  # Generate output every 1 hour
 p.runtime.output_file_mode = 'bin'
 
 params_timestep_size_reference = 30.0
-base_timestep_size = 128/p.runtime.space_res_spectral*300.0
+base_timestep_size = 128/p.runtime.space_res_spectral*1200.0
 
 # Parallelization
 nSpacePar = int(sys.argv[1]) if len(sys.argv) > 1 else p.platform_resources.num_cores_per_socket
