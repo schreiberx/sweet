ea981650468aee54725035ae15b2a2dcdf38c63a
SCHREIBER Martin <martin.schreiber@inria.fr>
2023-02-16 23:18:09 +0100

    added timex platform with llvm15

---
+++ mule/platforms/80_timex_llvm15/env_vars.sh
@@ -0,0 +1,24 @@
+
+#
+# Compiler environment
+#
+
+export PYTHON=python3
+export CC=clang-15
+export CXX=clang++-15
+export F90=gfortran
+
+
+#export MULE_LINK=$MULE_CXX
+
+#export MULE_MPICC=mpicc
+#export MULE_MPICXX=mpic++
+#export MULE_MPIF90=mpif90
+
+#export MULE_MPILINK=mpif90
+#export MULE_MPILIBS=stdc++
+
+
+#export MULE_CC_COMPILER=gcc
+#export MULE_CXX_COMPILER=gcc
+#export MULE_F90_COMPILER=gcc
