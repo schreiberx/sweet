4b53e345cd96261fea3703bd5f9f6566c01ef004
Thibaut Lunet <thibaut.lunet@tuhh.de>
2023-03-16 18:59:37 +0100

    TL: matplotlib formating

--- benchmarks_sphere/parallel_sdc/performance/01_sweetForDaniel/step4_process.py
+++ benchmarks_sphere/parallel_sdc/performance/01_sweetForDaniel/step4_process.py
@@ -6,6 +6,22 @@ from mule.postprocessing.JobsData import *
 from mule.postprocessing.JobsDataConsolidate import *
 from mule.plotting.Plotting import *
 
+plt.rc('font', size=12)
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams['axes.titlesize'] = 18
+plt.rcParams['axes.labelsize'] = 16
+plt.rcParams['xtick.labelsize'] = 16
+plt.rcParams['ytick.labelsize'] = 16
+plt.rcParams['xtick.major.pad'] = 5
+plt.rcParams['ytick.major.pad'] = 5
+plt.rcParams['axes.labelpad'] = 6
+plt.rcParams['markers.fillstyle'] = 'none'
+plt.rcParams['lines.markersize'] = 7.0
+plt.rcParams['lines.markeredgewidth'] = 1.5
+plt.rcParams['mathtext.fontset'] = 'cm'
+plt.rcParams['mathtext.rm'] = 'serif'
+plt.rcParams['figure.max_open_warning'] = 100
+
 sys.path.append('../')
 sys.path.pop()
 
