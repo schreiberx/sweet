101fc4d84c67313bcead628d196c7d87b9c12de0
keerthi <keerthigaddameedi@gmail.com>
2023-04-17 15:22:38 +0200

    Get an email from slurm only for beginning and end of a job

--- mule/platforms/50_linuxcluster_intel/JobPlatform.py
+++ mule/platforms/50_linuxcluster_intel/JobPlatform.py
@@ -105,7 +105,7 @@ def jobscript_get_header(jg : JobGeneration):
 #SBATCH --ntasks-per-node="""+str(p.num_ranks_per_node)+"""
 # the above is a good match for the
 # CooLMUC2 architecture.
-#SBATCH --mail-type=ALL 
+#SBATCH --mail-type=BEGIN,END 
 #SBATCH --mail-user="""+mule_user_email+"""
 #SBATCH --export=NONE 
 #SBATCH --time="""+time_str+"""
